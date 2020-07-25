38d1c30b4614ca379f679d03c1e700d854d5f3b1
Khinshan Khan <khanshan501@gmail.com>
2020-07-25 19:09:29 -0400

    404 page reimplemented

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import axios from "axios";
+
+const fetchData = async (url) =>
+  axios(`https://cors-anywhere.herokuapp.com/${url}`);
+
+const getComic = async (num) => {
+  const comic = await fetchData(`https://xkcd.com/${num}/info.0.json`);
+  return comic.data;
+};
+
+const getRandomComic = async () => {
+  const xkcdInfo = await fetchData("https://xkcd.com/info.0.json");
+  const num = Math.floor(Math.random() * xkcdInfo.data.num);
+  return getComic(num);
+};
 
 const NotFound = () => {
-  return <>404</>;
+  const [comicInfo, setComicInfo] = useState(null);
+
+  useEffect(() => {
+    const setInfo = async () => {
+      const comic = await getRandomComic();
+      setComicInfo(comic);
+    };
+    setInfo();
+  }, [setComicInfo]);
+
+  console.log(comicInfo);
+  return (
+    <>
+      <h1>404: Page not found</h1>
+      <p>Sorry, but the page could not be found.</p>
+      {comicInfo && (
+        <>
+          <h6>Enjoy this lovely xkcd (#{comicInfo.num}) comic as you wait.</h6>
+          <br />
+          <img src={comicInfo.img} alt={`XKCD Comic #${comicInfo.num}`} />
+        </>
+      )}
+      <br />
+      <Link to="/">Click Here to Return to Safety</Link>
+    </>
+  );
 };
 
 export default NotFound;
