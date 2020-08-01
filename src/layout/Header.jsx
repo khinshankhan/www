ac74070ce2fdc5bf9568fab836a8d5ddda0dff35
Khinshan Khan <khanshan501@gmail.com>
2020-08-01 19:11:32 -0400

    played with conditional elevation for appbar

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link as GatsbyLink } from "gatsby";
+import useScrollPosition from "src/hooks/useScrollPosition";
 import { HideOnScroll } from "src/components/Scroll";
 import AppBar from "@material-ui/core/AppBar";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
@@ -10,6 +11,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -29,12 +31,20 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = ({ siteTitle = "Shan", onToggleTheme, theme }) => {
   const classes = useStyles();
+  const [onTop, setOnTop] = React.useState(true);
+
+  useScrollPosition(({ _prevPos, currPos }) => {
+    setOnTop(currPos.y === 0 ? true : false);
+  });
 
   return (
     <>
       <div id="back-to-top-anchor"></div>
       <HideOnScroll>
-        <AppBar position="sticky" className={classes.appbar}>
+        <AppBar
+          position="sticky"
+          className={clsx(classes.appbar, onTop && classes.appbarUnelevated)}
+        >
           <Toolbar component="nav">
             <Typography variant="h6" className={classes.title}>
               <Link
