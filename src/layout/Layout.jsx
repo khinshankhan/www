dbde113b90dbb4e57cb394868517192dde6e00cd
Khinshan Khan <khanshan501@gmail.com>
2020-08-09 14:50:18 -0400

    greater control over content gutter spacing

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles, useTheme } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ScrollToTop } from "src/components/Scroll";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
@@ -21,15 +23,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const Gutters = ({ xs, classes, children }) => {
+  return xs ? (
+    <Container className={classes.main}>{children}</Container>
+  ) : (
+    <Grid container>
+      <Grid item xs={false} sm={1} />
+      <Grid item xs={12} sm={10}>
+        {children}
+      </Grid>
+      <Grid item xs={false} sm={1} />
+    </Grid>
+  );
+};
 const Layout = ({ content, toggleTheme, themeType }) => {
+  const theme = useTheme();
   const classes = useStyles();
+  const xs = useMediaQuery(theme.breakpoints.only("xs"));
 
+  console.log({ xs });
   return (
     <>
       <CssBaseline />
       <div className={classes.root}>
         <Header toggleTheme={toggleTheme} themeType={themeType} />
-        <Container className={classes.main}>{content}</Container>
+        <Gutters xs={xs} classes={classes}>
+          {content}
+        </Gutters>
         <ScrollToTop>
           <Fab color="secondary" size="small" aria-label="scroll back to top">
             <KeyboardArrowUpIcon />
