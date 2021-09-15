f932977e2167af8e3560673de6961246cf3c3fb0
Khinshan Khan <khanshan501@gmail.com>
2021-09-14 21:44:46 -0400

    added precommit linting

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "*.{js,ts,tsx}": [
+    `eslint --ignore-path .gitignore --ignore-path .prettierignore --fix`,
+  ],
+  "*.{md,mdx,json,yaml}": [`prettier "**/*.{md,mdx,json,yaml}" --write`],
+};
