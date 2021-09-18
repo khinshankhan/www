094514feb165a06c4a2d9f7296b6433371dbc0cf
Khinshan Khan <khanshan501@gmail.com>
2021-09-17 23:12:06 -0400

    initial attempt at logo

--- www/src/layouts/Shared/Header/Desktop.tsx
+++ www/src/layouts/Shared/Header/Desktop.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ToggleMode from "src/components/ToggleMode";
-import { Flex, Text, Button, Stack } from "@chakra-ui/react";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import Logo from "src/assets/logo";
 
 const Desktop = (): JSX.Element => (
   <Flex align="center" justify="space-between" p="4">
-    <Text>shannyboi</Text>
+    <Logo />
     <Stack isInline spacing="3">
       <Button variant="ghost">About</Button>
       <Button variant="ghost">Portfolio</Button>
       <Button variant="ghost">Writing</Button>
+      <ToggleMode />
       <ToggleMode />
       <ToggleMode />
     </Stack>
