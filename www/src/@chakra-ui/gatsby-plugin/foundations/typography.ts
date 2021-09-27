f09df98dd96442cfa9ebb01ace477607bec0b655
Khinshan Khan <khanshan501@gmail.com>
2021-09-26 22:36:08 -0400

    initial attempt at typography

---
+++ www/src/@chakra-ui/gatsby-plugin/foundations/typography.ts
@@ -0,0 +1,37 @@
+import { ThemeOverride } from "@chakra-ui/react";
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+const fontWeights: ThemeOverride["fontWeights"] = {
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+};
+
+const fonts: ThemeOverride["fonts"] = {
+  // copy chakra-ui
+  heading: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  // copy chakra-ui
+  body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  // copy chakra-ui
+  mono: `SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+};
+
+// 1.250 Major Third Type Scale https://type-scale.com/ base 16px
+const fontSizes: ThemeOverride["fontSizes"] = {
+  xs: `0.64rem`,
+  sm: `0.8rem`,
+  md: `1rem`, // 16px
+  lg: `1.25rem`,
+  xl: `1.563rem`,
+  "2xl": `1.953rem`,
+  "3xl": `2.441rem`,
+  "4xl": `3.052rem`,
+  "5xl": `3.815rem`,
+  "6xl": `4.768rem`,
+  "7xl": `5.96rem`,
+  "8xl": `7.451rem`,
+  "9xl": `9.313rem`,
+};
+
+export { fontWeights, fonts, fontSizes };
---
+++ www/src/@chakra-ui/gatsby-plugin/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+// Foundational style overrides
+import { fontWeights, fonts, fontSizes } from "./foundations/typography";
+
+// Custom breakpoints
+const breakpoints = createBreakpoints({
+  sm: `640px`,
+  md: `768px`,
+  lg: `1024px`,
+  xl: `1280px`,
+  "2xl": `1536px`,
+});
+
+const theme = extendTheme({
+  breakpoints,
+  fontWeights,
+  fonts,
+  fontSizes,
+  config: {
+    initialColorMode: `dark`,
+    useSystemColorMode: true,
+  },
+});
+
+export default theme;
