1405cb248118009774d6566e52550fa2e29a0550
Khinshan Khan <khanshan501@gmail.com>
2021-10-16 16:10:30 -0400

    exploring prose template

--- www/src/templates/prose.tsx
+++ www/src/templates/prose.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { MDXProvider } from "@mdx-js/react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { components } from "src/components/mdx";
+import Layout from "src/layouts/Single";
+import headings from "src/components/mdx/h";
 
 const Prose = ({ data, pageContext, location }) => {
   console.log({ data, pageContext, location });
-  return <>Hi</>;
+
+  const { mdx } = data;
+  const { body, frontmatter } = mdx;
+
+  return (
+    <Layout>
+      <headings.h1>{frontmatter.title}</headings.h1>
+      <br />
+      <MDXProvider components={components}>
+        <MDXRenderer>{body}</MDXRenderer>
+      </MDXProvider>
+    </Layout>
+  );
 };
 
 export default Prose;
+
+export const query = graphql`
+  query Prose($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      body
+    }
+  }
+`;
