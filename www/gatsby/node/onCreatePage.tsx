d6d7d1f533d56d931672d6227c3c0c5c9dafe022
Khinshan Khan <khanshan501@gmail.com>
2021-10-16 14:43:56 -0400

    fix non-deterministic routing behavior warnings

--- www/gatsby/node/onCreatePage.tsx
+++ www/gatsby/node/onCreatePage.tsx
@@ -1,20 +1,5 @@
 import { GatsbyNode } from "gatsby";
 
-const replacePath = (_path: string) =>
-  _path === `/` ? _path : _path.replace(/\/$/, ``);
-
-const onCreatePage: GatsbyNode["onCreatePage"] = ({ page, actions }) => {
-  const { createPage, deletePage } = actions;
-
-  return new Promise((resolve) => {
-    const oldPage = { ...page };
-    page.path = replacePath(page.path);
-    if (page.path !== oldPage.path) {
-      deletePage(oldPage);
-      createPage(page);
-    }
-    resolve();
-  });
-};
+const onCreatePage: GatsbyNode["onCreatePage"] = () => {};
 
 export default onCreatePage;
