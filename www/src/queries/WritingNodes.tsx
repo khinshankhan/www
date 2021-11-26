3e14f17e0d6ea4a16615df01229768858ea3432e
Khinshan Khan <khanshan501@gmail.com>
2021-11-25 23:30:13 -0500

    add in rough types for queries

---
+++ www/src/queries/WritingNodes.tsx
@@ -0,0 +1,47 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+export type IWritingNode = {
+  frontmatter: {
+    title: string;
+    planted: string;
+    humanDatePlanted?: string;
+  };
+  slug: string;
+  excerpt: string;
+  timeToRead: number;
+  id: string;
+};
+
+export type IWritingNodeWrapper = {
+  node: IWritingNode;
+};
+
+export type IListWritingNodes = {
+  allMdx: {
+    edges: IWritingNodeWrapper[];
+  };
+};
+
+export const ListWritingNodes = () =>
+  useStaticQuery(graphql`
+    {
+      allMdx(
+        filter: { fields: { source: { eq: "writing" } } }
+        sort: { fields: frontmatter___planted, order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              planted
+              humanDatePlanted: planted(formatString: "MMM Do, YYYY")
+            }
+            slug
+            excerpt(truncate: true, pruneLength: 300)
+            timeToRead
+            id
+          }
+        }
+      }
+    }
+  `) as IListWritingNodes;
