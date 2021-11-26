338e48a261ab03e937416064a60104605b570c19
Khinshan Khan <khanshan501@gmail.com>
2021-11-25 19:34:54 -0500

    use uniform font sizes

--- www/src/constants/fonts.ts
+++ www/src/constants/fonts.ts
@@ -2,3 +2,5 @@
 export const defaultFontHeading = `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
 export const defaultFontBody = `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
 export const defaultFontMono = `SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`;
+
+export const fontSizes = [`mdlg`, `mdlg`, `mdlg`, `lgxl`, `lgxl`, `xl2xl`];
