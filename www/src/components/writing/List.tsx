5476f9b40be12fcb1a1fe628c3cb010d4b43fbdb
Khinshan Khan <khanshan501@gmail.com>
2021-11-26 22:59:36 -0500

    better focus colors for articles

--- www/src/components/writing/List.tsx
+++ www/src/components/writing/List.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Box, Text, LinkOverlay, LinkBox } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  LinkOverlay,
+  LinkBox,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import headings from "src/components/mdx/headings";
 import Link from "src/components/mdx/Link";
 import { IWritingNodeWrapper, IWritingNode } from "src/queries/WritingNodes";
@@ -9,33 +15,48 @@ type IWritingListItemProps = {
   [key: string]: any;
 };
 
-const WritingListItem = ({ node, ...props }: IWritingListItemProps) => (
-  <LinkBox
-    pb="5"
-    _hover={{
-      color: `gray.400`,
-    }}
-    {...props}
-  >
-    <Box as="article" pl="5" pb="3" borderWidth="1px" rounded="md">
-      <headings.h3
-        color="#BB72EC"
-        _hover={{
-          color: `#F40057`,
-          textDecoration: `none`,
-        }}
+const WritingListItem = ({ node, ...props }: IWritingListItemProps) => {
+  const bg = useColorModeValue(`#FFFFFF`, `#1A202C`);
+  const bgHover = useColorModeValue(`#DDDDDD`, `#1E2430`);
+  const colorHover = useColorModeValue(`gray.500`, `gray.400`);
+
+  return (
+    <LinkBox
+      pb="5"
+      _hover={{
+        color: colorHover,
+      }}
+      {...props}
+    >
+      <Box
+        as="article"
+        pl="5"
+        pb="3"
+        borderWidth="1px"
+        rounded="md"
+        bg={bg}
+        _hover={{ background: bgHover }}
       >
-        <LinkOverlay as={Link} href={node.slug}>
-          {node.frontmatter.title}
-        </LinkOverlay>
-      </headings.h3>
-      <Box as="time" dateTime={node.frontmatter.planted}>
-        {node.frontmatter.humanDatePlanted} &middot; {node.timeToRead} min read
+        <headings.h3
+          color="#BB72EC"
+          _hover={{
+            color: `#F40057`,
+            textDecoration: `none`,
+          }}
+        >
+          <LinkOverlay as={Link} href={node.slug}>
+            {node.frontmatter.title}
+          </LinkOverlay>
+        </headings.h3>
+        <Box as="time" dateTime={node.frontmatter.planted}>
+          {node.frontmatter.humanDatePlanted} &middot; {node.timeToRead} min
+          read
+        </Box>
+        <Text>{node.excerpt}</Text>
       </Box>
-      <Text>{node.excerpt}</Text>
-    </Box>
-  </LinkBox>
-);
+    </LinkBox>
+  );
+};
 
 type IWritingListProps = {
   nodes: IWritingNodeWrapper[];
