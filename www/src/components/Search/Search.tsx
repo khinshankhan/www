bb044783251684319d2034a434b77af01942806d
Khinshan Khan <khanshan501@gmail.com>
2021-11-28 17:09:53 -0500

    filter posts based on tags selected

--- www/src/components/Search/Search.tsx
+++ www/src/components/Search/Search.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Box } from "@chakra-ui/react";
+import { IWritingNodeWrapper } from "src/queries/WritingNodes";
 import DisplayTags from "./DisplayTags";
+import useFilterNodes from "./useFilterNodes";
 
-const Search = ({ ...props }) => (
-  <Box {...props}>
-    <DisplayTags />
-  </Box>
-);
+interface ISearchProps {
+  nodes: IWritingNodeWrapper[];
+  setNodes: Dispatch<SetStateAction<IWritingNodeWrapper[]>>;
+  [key: string]: any;
+}
+
+const Search = ({ nodes, setNodes, ...props }: ISearchProps) => {
+  useFilterNodes({ nodes, setNodes });
+
+  return (
+    <Box {...props}>
+      <DisplayTags />
+    </Box>
+  );
+};
 
 export default Search;
---
+++ www/src/components/Search/useFilterNodes.tsx
@@ -0,0 +1,27 @@
+import { useEffect, Dispatch, SetStateAction } from "react";
+import { useSearchInfo } from "src/contexts/SearchInfo";
+import { IWritingNodeWrapper } from "src/queries/WritingNodes";
+
+interface IUseFilterNodes {
+  nodes: IWritingNodeWrapper[];
+  setNodes: Dispatch<SetStateAction<IWritingNodeWrapper[]>>;
+}
+
+const useFilterNodes = ({ nodes, setNodes }: IUseFilterNodes) => {
+  const { selectedTags } = useSearchInfo();
+
+  useEffect(() => {
+    let newNodes = nodes;
+
+    if (selectedTags.size !== 0) {
+      const tags = [...selectedTags];
+      newNodes = nodes.filter((node) =>
+        tags.every((tag) => node.node.frontmatter.categories.includes(tag))
+      );
+    }
+
+    setNodes(newNodes);
+  }, [nodes, setNodes, selectedTags]);
+};
+
+export default useFilterNodes;
