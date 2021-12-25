ded474609183b849a4f1fb86502db107cf17760d
Khinshan Khan <khanshan501@gmail.com>
2021-12-25 16:09:25 -0500

    clean up foundations structure

--- www/src/@chakra-ui/gatsby-plugin/foundations/typography.ts
+++ www/src/@chakra-ui/gatsby-plugin/foundations/typography.ts
@@ -1,38 +1,76 @@
 import { defaultFontHeading, defaultFontBody, defaultFontMono } from "constants/fonts";
 import { ThemeOverride } from "@chakra-ui/react";
 
+const letterSpacings: ThemeOverride["letterSpacings"] = {
+  tighter: `-0.05em`,
+  tight: `-0.025em`,
+  normal: `0`,
+  wide: `0.025em`,
+  wider: `0.05em`,
+  widest: `0.1em`,
+};
+
+const lineHeights: ThemeOverride["lineHeights"] = {
+  normal: `normal`,
+  none: 1,
+  shorter: 1.25,
+  short: 1.375,
+  base: 1.5,
+  tall: 1.625,
+  taller: `2`,
+  "3": `.75rem`,
+  "4": `1rem`,
+  "5": `1.25rem`,
+  "6": `1.5rem`,
+  "7": `1.75rem`,
+  "8": `2rem`,
+  "9": `2.25rem`,
+  "10": `2.5rem`,
+};
+
 // https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
 const fontWeights: ThemeOverride["fontWeights"] = {
+  hairline: 100,
+  thin: 200,
+  light: 300,
   normal: 400,
   medium: 500,
   semibold: 600,
   bold: 700,
+  extrabold: 800,
+  black: 900,
 };
 
 const fonts: ThemeOverride["fonts"] = {
-  heading: `'Crimson Pro', ui-serif, Cambria, ${defaultFontHeading}`,
-  body: `'Crimson Text', ui-serif, ${defaultFontBody}`,
+  heading: defaultFontHeading,
+  body: defaultFontBody,
   mono: defaultFontMono,
 };
 
-// 1.250 Major Third Type Scale https://type-scale.com/ base 16px
+// 1.250 Major Third Type Scale,
+// https://type-scale.com/
 const fontSizes: ThemeOverride["fontSizes"] = {
-  xs: `0.64rem`,
-  sm: `0.8rem`,
-  md: `1rem`, // 16px
-  mdlg: `1.125rem`,
-  lg: `1.25rem`,
-  lgxl: `1.4065rem`,
-  xl: `1.563rem`,
-  xl2xl: `1.758rem`,
-  "2xl": `1.953rem`,
-  "3xl": `2.441rem`,
-  "4xl": `3.052rem`,
-  "5xl": `3.815rem`,
-  "6xl": `4.768rem`,
-  "7xl": `5.96rem`,
-  "8xl": `7.451rem`,
-  "9xl": `9.313rem`,
+  xs: `0.75rem`,
+  sm: `0.875rem`,
+  md: `1rem`,
+  lg: `1.125rem`,
+  xl: `1.25rem`,
+  "2xl": `1.5rem`,
+  "3xl": `1.875rem`,
+  "4xl": `2.25rem`,
+  "5xl": `3rem`,
+  "6xl": `3.75rem`,
+  "7xl": `4.5rem`,
+  "8xl": `6rem`,
+  "9xl": `8rem`,
 };
 
-export { fontWeights, fonts, fontSizes };
+const typography: ThemeOverride["typography"] = {
+  letterSpacings,
+  lineHeights,
+  fontWeights,
+  fonts,
+  fontSizes,
+};
+
+export default typography;
