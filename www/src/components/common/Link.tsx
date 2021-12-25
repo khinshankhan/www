986c8472d4acf1e32d2efa8e3f614542e764ab06
Khinshan Khan <khanshan501@gmail.com>
2021-12-25 15:57:13 -0500

    add portfolio page placeholder

--- www/src/components/common/Link.tsx
+++ www/src/components/common/Link.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { chakra } from "@chakra-ui/react";
 import { Link as GatsbyLink } from "gatsby";
-import { chakra, ChakraComponent } from "@chakra-ui/react";
 
 const InternalLink = chakra(GatsbyLink, {
   baseStyle: {
@@ -29,17 +29,13 @@ export type ILinkProps = {
   /*   children: [{ type: string; value: string }]; */
 };
 
-type LinkComponentType =
-  | ChakraComponent<typeof GatsbyLink, {}>
-  | ChakraComponent<ChakraComponent<"a", {}>, {}>;
-
 const Link = ({ href, ...props }: ILinkProps) => {
   const foreignLink = href && href.startsWith(`http`);
-  const LinkComponent: LinkComponentType = foreignLink ? ExternalLink : InternalLink;
+  const LinkComponent = foreignLink ? ExternalLink : InternalLink;
   const target = foreignLink ? `_blank` : undefined;
   const rel = foreignLink ? `noreferrer noopener` : undefined;
 
-  return <LinkComponent to={href} href={href} target={target} rel={rel} {...props} />;
+  return <LinkComponent to={href} target={target} rel={rel} {...props} />;
 };
 
 export default Link;
