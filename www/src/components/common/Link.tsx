27a8581cab8a8fada91670c012055ab3a682bed3
Khinshan Khan <khanshan501@gmail.com>
2021-12-25 20:46:01 -0500

    links have so many edge cases

--- www/src/components/common/Link.tsx
+++ www/src/components/common/Link.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { chakra, Icon, Link as ChakraLink } from "@chakra-ui/react";
 import { Link as GatsbyLink } from "gatsby";
-import { BsBoxArrowUpRight as BoxArrowUpRight } from "react-icons/bs";
+import {
+  BsBoxArrowUpRight as BoxArrowUpRight,
+  BsBoxArrowInDown as BoxArrowInDown,
+} from "react-icons/bs";
 
 // TODO: handle title
 export type ILinkProps = {
@@ -11,14 +14,27 @@ export type ILinkProps = {
 };
 
 const Link = ({ href, children, ...props }: ILinkProps) => {
-  const foreignLink = href && href.startsWith(`http`);
-  const LinkComponent = foreignLink ? chakra.a : GatsbyLink;
-  const target = foreignLink ? `_blank` : undefined;
-  const rel = foreignLink ? `noreferrer noopener` : undefined;
+  // TODO: decide how to handle href-less links
+  // NOTE: this currently treats relative paths as external links as well
+  const foreign = !(href && /^\/(?!\/)/.test(href));
+  const file = /\.[0-9a-z]+$/i.test(href);
+  const relative = !(foreign || file);
+
+  const LinkComponent = relative ? GatsbyLink : chakra.a;
+  const target = foreign ? `_blank` : undefined;
+  const rel = foreign ? `noreferrer noopener` : undefined;
+  const VisualIcon = !foreign && file ? BoxArrowInDown : BoxArrowUpRight;
 
   return (
-    <ChakraLink as={LinkComponent} to={href} href={href} target={target} rel={rel} {...props}>
-      {children} {foreignLink && <Icon as={BoxArrowUpRight} />}
+    <ChakraLink
+      as={LinkComponent}
+      to={(relative && href) || undefined}
+      href={(!relative && href) || undefined}
+      target={target}
+      rel={rel}
+      {...props}
+    >
+      {children} {!relative && <Icon as={VisualIcon} />}
     </ChakraLink>
   );
 };
