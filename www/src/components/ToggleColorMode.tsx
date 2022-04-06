36efb464e5fe6ce6ac20e3bdd963e1818d433a4b
Khinshan Khan <khanshan501@gmail.com>
2022-04-05 21:59:43 -0400

    improve color toggle by getting rid of react-toggle-dark-mode

--- www/src/components/ToggleColorMode.tsx
+++ www/src/components/ToggleColorMode.tsx
@@ -1,31 +1,22 @@
 import React from "react";
-import { useColorMode, useColorModeValue, IconButton } from "@chakra-ui/react";
-import { DarkModeSwitch } from "react-toggle-dark-mode";
+import { useColorMode, IconButton } from "@chakra-ui/react";
+import { CgMoon as Moon, CgSun as Sun } from "react-icons/cg";
 
-interface IToggleColorModeSvgProps {
-  colorMode: `dark` | `light`;
-  toggleColorMode?: () => void;
-  size: number;
-}
-const ToggleColorModeSvg = ({
-  colorMode,
-  toggleColorMode = () => {},
-  size,
-}: IToggleColorModeSvgProps): JSX.Element => (
-  <DarkModeSwitch checked={colorMode === `dark`} onChange={toggleColorMode} size={size} />
-);
 interface IToggleColorModeProps {
   size?: number;
 }
 const ToggleColorMode = ({ size = 24 }: IToggleColorModeProps) => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const text = useColorModeValue(`dark`, `light`);
+  const isDark = colorMode === `dark`;
+  const text = isDark ? `dark` : `light`;
+
+  const ColorIcon = isDark ? Moon : Sun;
   return (
     <IconButton
       aria-label={`Switch to ${text} mode`}
       variant="ghost"
       onClick={toggleColorMode}
-      icon={<ToggleColorModeSvg colorMode={colorMode} size={size} />}
+      icon={<ColorIcon size={size} />}
     />
   );
 };
