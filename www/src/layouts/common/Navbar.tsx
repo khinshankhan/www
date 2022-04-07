d59b81c80f7bb1a52c35781284481e165183e750
Khinshan Khan <khanshan501@gmail.com>
2022-04-06 22:11:41 -0400

    mobile navbar

--- www/src/layouts/common/Navbar.tsx
+++ www/src/layouts/common/Navbar.tsx
@@ -1,66 +1,115 @@
 import React from "react";
-import { useColorModeValue, Box, Container, Flex, HStack, IconButton } from "@chakra-ui/react";
-import { FaCookie as Cookie } from "react-icons/fa";
+import {
+  useColorModeValue,
+  useDisclosure,
+  Box,
+  Container,
+  Collapse,
+  Flex,
+  HStack,
+  IconButton,
+  VStack,
+} from "@chakra-ui/react";
+import useMobile from "src/hooks/useMobile";
 import Logo from "src/assets/Logo";
 import Heading from "src/components/common/Heading";
 import Link from "src/components/common/Link";
+import { FaCookie as Cookie } from "react-icons/fa";
+import { GiHamburgerMenu as Hamburger } from "react-icons/gi";
+import { AiOutlineClose as CloseIcon } from "react-icons/ai";
 import { ToggleColorMode, ToggleDirection } from "src/components/Toggles";
 
 // TODO: replace this with a call to gatsby to get config based values
 const MENU_ITEMS = [
   {
-    title: `About`,
-    href: `/about`,
+    title: "About",
+    href: "/about",
   },
   {
-    title: `Writing`,
-    href: `/writing`,
+    title: "Writing",
+    href: "/writing",
   },
   {
-    title: `Portfolio`,
-    href: `/portfolio`,
+    title: "Portfolio",
+    href: "/portfolio",
   },
   {
-    title: `Contact`,
-    href: `/contact`,
+    title: "Contact",
+    href: "/contact",
   },
 ];
 
-export const Navbar = () => (
-  <Container as="nav" variant="page" id="navbar" mb="4">
-    <Flex
-      minH={`75px`}
-      pt="6"
-      pb="8"
-      borderBottom={1}
-      borderStyle="solid"
-      borderColor={useColorModeValue(`gray.200`, `white`)}
-      align="center"
-    >
-      <Flex flex={{ base: 1 }} justify="start">
-        <Logo />
-      </Flex>
+export const Navbar = () => {
+  const isMobile = useMobile();
+  const { isOpen, onToggle } = useDisclosure();
+  const dividerColor = useColorModeValue("gray.200", "white");
 
-      <Box as="menu" id="menu">
-        <HStack flex={0} justify={`flex-end`} direction={`row`} spacing={6}>
-          <HStack as="ul" listStyleType="none" spacing={4}>
-            {MENU_ITEMS.map((item) => (
-              <Heading.h3 as="li" key={item.title} variant="link">
-                {/* TODO: replace href with item.href once pages are ready */}
-                <Link href={`#`}>{item.title}</Link>
-              </Heading.h3>
-            ))}
-          </HStack>
+  const MenuIcon = isOpen ? CloseIcon : Hamburger;
+  const MenuStack = isMobile ? VStack : HStack;
+  const Menu = () => (
+    <MenuStack flex={0} justify={"flex-end"} direction={"row"} spacing={6}>
+      <MenuStack as="ul" listStyleType="none" spacing={4}>
+        {MENU_ITEMS.map((item) => (
+          <Heading.h3 as="li" key={item.title} variant="link">
+            {/* TODO: replace href with item.href once pages are ready */}
+            <Link href={"#"}>{item.title}</Link>
+          </Heading.h3>
+        ))}
+      </MenuStack>
 
-          <HStack>
-            <ToggleColorMode />
-            <ToggleDirection />
-            <IconButton aria-label={`View storage policy`} variant="ghost" icon={<Cookie />} />
-          </HStack>
-        </HStack>
-      </Box>
-    </Flex>
-  </Container>
-);
+      <HStack>
+        <ToggleColorMode />
+        <ToggleDirection />
+        <IconButton aria-label={`View storage policy`} variant="ghost" icon={<Cookie />} />
+      </HStack>
+    </MenuStack>
+  );
+
+  return (
+    <>
+      <Container as="nav" variant="page" id="navbar" mb="4">
+        <Flex
+          minH="75px"
+          pt="6"
+          pb="8"
+          borderBottom={1}
+          borderStyle="solid"
+          borderColor={dividerColor}
+          align="center"
+        >
+          <Flex flex={{ base: 1 }} justify="start">
+            <Logo />
+          </Flex>
+
+          <Box as="menu" id="menu">
+            {isMobile ? (
+              <IconButton
+                aria-label={`Toggle Navigation`}
+                onClick={onToggle}
+                variant="ghost"
+                icon={<MenuIcon fontSize={`1.563rem`} />}
+              />
+            ) : (
+              <Box as="menu" id="menu">
+                <Menu />
+              </Box>
+            )}
+          </Box>
+        </Flex>
+      </Container>
+
+      {isMobile && (
+        <Container as="menu" variant="page" id="menu" mb="4">
+          <Collapse in={isOpen} animateOpacity>
+            <Menu />
+            {isOpen && (
+              <Flex pt="4" borderBottom={1} borderStyle="solid" borderColor={dividerColor} />
+            )}
+          </Collapse>
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default Navbar;
