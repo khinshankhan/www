93e2c14cb9219305a9e3b96e0ffe692595b12415
Khinshan Khan <khanshan501@gmail.com>
2022-04-09 20:21:57 -0400

    fill out copyright info

--- www/src/layouts/common/Footer/Footer.tsx
+++ www/src/layouts/common/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useColorModeValue, Container, Stack } from "@chakra-ui/react";
+import Copyright from "./Copyright";
+import SocialMediaLinks from "./SocialMediaLinks";
 
 export const Footer = () => {
   const dividerColor = useColorModeValue(`gray.200`, `white`);
@@ -7,16 +9,16 @@ export const Footer = () => {
   return (
     <Container variant="page" as="footer" id="footer">
       <Stack
-        direction={{ base: `column-reverse`, md: `row` }}
+        direction={{ base: `column`, md: `row` }}
         justifyContent="space-between"
         alignItems="center"
-        pt="5"
+        pt="4"
         borderTop={1}
         borderStyle="solid"
         borderColor={dividerColor}
       >
-        <div>c</div>
-        <div>s</div>
+        <Copyright />
+        <SocialMediaLinks />
       </Stack>
     </Container>
   );
