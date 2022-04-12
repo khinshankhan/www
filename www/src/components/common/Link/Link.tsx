a3d80f778b5018a91adb99f579d09befcfc6399c
Khinshan Khan <khanshan501@gmail.com>
2022-04-11 22:05:26 -0400

    settle on icons and colors for file links

--- www/src/components/common/Link/Link.tsx
+++ www/src/components/common/Link/Link.tsx
@@ -28,19 +28,21 @@ export const Link = ({ href, isFile = undefined, children, ...props }: ILinkProp
   const LinkComponent = relative ? InternalLink : ExternalLink;
   // TODO: potentially have an icon for every type of link
   // and have aria text for the icon for accessibility purposes
-  const VisualIcon = sameOrigin && file ? ArrowDownSquare : ArrowUpRightSquare;
+  const VisualIcon = file ? ArrowDownSquare : ArrowUpRightSquare;
 
-  const linkProps = {
-    href,
-    ...(!relative && { target: `_blank`, rel: `noreferrer noopener` }),
-  };
   return (
     <>
-      <LinkComponent {...linkProps} {...props}>
+      <LinkComponent
+        href={href}
+        target={!relative ? `_blank` : undefined}
+        rel={!relative ? `noreferrer noopener` : undefined}
+        variant={sameOrigin ? `internal` : `external`}
+        {...props}
+      >
         {children}
       </LinkComponent>
       {!relative && (
-        <Text as="sup" color="inherit" textDecoration="none">
+        <Text as="sup" color={sameOrigin ? `internal` : `external`} textDecoration="none">
           {` `}
           <Icon as={VisualIcon} boxSize="0.9rem" />
         </Text>
