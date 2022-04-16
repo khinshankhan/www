6e46b8742380c596d597450c2f5ccd86457d217e
Khinshan Khan <khanshan501@gmail.com>
2022-04-15 23:55:47 -0400

    sane typography defaults, need to choose custom fonts for text

---
+++ www/constants/fonts.ts
@@ -0,0 +1,16 @@
+import { ThemeOverride } from "@chakra-ui/react";
+
+// fairly safe fallback in case something goes wrong with fonts
+// default fonts copied from chakra ui
+// https://github.com/chakra-ui/chakra-ui/blob/main/packages/theme/src/foundations/typography.ts
+export const defaultFonts: ThemeOverride["fonts"] = {
+  heading: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  mono: `SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+};
+
+export const fonts: ThemeOverride["fonts"] = {
+  heading: `ui-serif, Cambria, ${defaultFonts.heading}`,
+  body: `ui-serif, ${defaultFonts.body}`,
+  mono: defaultFonts.mono,
+};
--- www/src/@chakra-ui/gatsby-plugin/foundations/index.ts
+++ www/src/@chakra-ui/gatsby-plugin/foundations/index.ts
@@ -1,10 +1,12 @@
 import { ThemeOverride } from "@chakra-ui/react";
 import colors from "./colors";
 import semanticTokens from "./semantic-tokens";
+import typography from "./typography";
 
 const foundations: ThemeOverride["foundations"] = {
   colors,
   semanticTokens,
+  ...typography,
 };
 
 export default foundations;
