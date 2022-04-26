becec09be06f2ee1f9cb285ca1e4a93b83b1894c
Khinshan Khan <khanshan501@gmail.com>
2022-04-26 16:14:11 -0400

    homepage

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,56 +1,30 @@
 import React from "react";
-import { Text } from "@chakra-ui/react";
-import { Heading, Link } from "src/components/common";
-import Layout from "src/layouts/Page";
+import { chakra, Center, Text, VStack } from "@chakra-ui/react";
+import { Heading } from "src/components/common";
+import Layout from "src/components/layouts/Centered";
 
+// TODO: fetch name from site meta
 const Index = () => (
   <Layout>
-    <Heading.h1Anchor href="/">Anchor Heading</Heading.h1Anchor>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ipsum sed justo tempor
-      pulvinar vel porttitor nisi. Cras ut ultrices leo, sit amet fringilla orci. Donec pellentesque
-      nunc at magna placerat ornare. Quisque iaculis nunc erat, vitae fringilla leo dapibus id. Cras
-      ut rhoncus leo, eget rhoncus eros. Aenean non finibus orci. Vivamus imperdiet nisl sit amet
-      tellus pharetra, eu placerat justo fermentum. Cras hendrerit elementum lorem, eget sodales
-      tellus egestas nec. Sed vel nulla augue. Vivamus nibh augue, accumsan id lobortis sit amet,
-      molestie at nulla. Pellentesque consequat condimentum elit vel rutrum. Phasellus eleifend urna
-      lacus, vel pellentesque ante semper sed.
-    </Text>
+    <Center as={Heading.h1} align="center" fontSize={[`4xl`, null, null, null, `5xl`]}>
+      <VStack>
+        <chakra.span>Hello hello 👋</chakra.span>
+        <chakra.span>I'm Khinshan!</chakra.span>
+      </VStack>
+    </Center>
 
-    <Heading.h1>
-      <Link href="/">Internal Link</Link>
-    </Heading.h1>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ipsum sed justo tempor
-      pulvinar vel porttitor nisi. Cras ut ultrices leo, sit amet fringilla orci. Donec pellentesque
-      nunc at magna placerat ornare.
-    </Text>
+    <Center as={Heading.h2} align="center" fontSize={[`3xl`, null, null, null, `4xl`]}>
+      I'm a software engineer.
+    </Center>
 
-    <Heading.h1>
-      <Link href="https://google.com">External Link</Link>
-    </Heading.h1>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ipsum sed justo tempor
-      pulvinar vel porttitor nisi. Cras ut ultrices leo, sit amet fringilla orci. Donec pellentesque
-      nunc at magna placerat ornare.
-    </Text>
-
-    <Heading.h1>
-      <Link href="https://google.com/hello.pdf">External File</Link>
-    </Heading.h1>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ipsum sed justo tempor
-      pulvinar vel porttitor nisi. Cras ut ultrices leo, sit amet fringilla orci. Donec pellentesque
-      nunc at magna placerat ornare.
-    </Text>
-
-    <Heading.h1>
-      <Link href="/hello.pdf">Internal File</Link>
-    </Heading.h1>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ipsum sed justo tempor
-      pulvinar vel porttitor nisi. Cras ut ultrices leo, sit amet fringilla orci. Donec pellentesque
-      nunc at magna placerat ornare.
+    <Text align="center" fontSize={`xl`}>
+      Wasn't too sure what to put on a homepage, so I put what would interest me: this site
+      currently has{` `}
+      <Text display="inline-block" color="internal" fontSize="inherit">
+        0
+      </Text>
+      {` `}
+      easter eggs!
     </Text>
   </Layout>
 );
