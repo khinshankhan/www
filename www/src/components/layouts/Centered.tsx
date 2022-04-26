7eaba179a0b275bb5170015365ae60c63bec9778
Khinshan Khan <khanshan501@gmail.com>
2022-04-26 18:41:13 -0400

    add faux breakpoint for centered max characters

--- www/src/components/layouts/Centered.tsx
+++ www/src/components/layouts/Centered.tsx
@@ -8,19 +8,14 @@ interface ICenteredLayoutProps {
 }
 
 const CenteredLayout = ({ children }: ICenteredLayoutProps) => {
-  const { isMobile } = useMobile();
+  const { isExtraSmall, isMobile } = useMobile();
   const fromTop = isMobile ? `0%` : `7%`;
+  const maxCh = isExtraSmall ? `32ch` : [`50ch`, `80ch`];
 
   return (
     <PageLayout minH={[`70vh`, null, `75vh`, null, null, `76vh`]}>
       <br />
-      <Container
-        as={VStack}
-        position="relative"
-        mt={fromTop}
-        spacing={[10]}
-        maxW={[`50ch`, `80ch`]}
-      >
+      <Container as={VStack} position="relative" mt={fromTop} spacing={[10]} maxW={maxCh}>
         {children}
       </Container>
       <br />
