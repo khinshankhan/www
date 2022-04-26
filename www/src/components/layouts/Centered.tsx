672376ebee6fb224e917708b2c5434a6274a04f2
Khinshan Khan <khanshan501@gmail.com>
2022-04-26 16:12:53 -0400

    better vertical centering

--- www/src/components/layouts/Centered.tsx
+++ www/src/components/layouts/Centered.tsx
@@ -1,18 +1,31 @@
 import React, { ReactNode } from "react";
 import { Container, VStack } from "@chakra-ui/react";
+import useMobile from "src/hooks/useMobile";
 import PageLayout from "./Page";
 
 interface ICenteredLayoutProps {
   children?: ReactNode;
 }
 
-const CenteredLayout = ({ children }: ICenteredLayoutProps) => (
-  <PageLayout minH={[`70vh`, null, `75vh`, null, null, `76vh`]}>
-    <br />
-    <Container as={VStack} position="relative" mt={[`5%`]} spacing={[10]} maxW={[`50ch`, `80ch`]}>
-      {children}
-    </Container>
-  </PageLayout>
-);
+const CenteredLayout = ({ children }: ICenteredLayoutProps) => {
+  const mobilep = useMobile();
+  const fromTop = mobilep ? `0%` : `7%`;
+
+  return (
+    <PageLayout minH={[`70vh`, null, `75vh`, null, null, `76vh`]}>
+      <br />
+      <Container
+        as={VStack}
+        position="relative"
+        mt={fromTop}
+        spacing={[10]}
+        maxW={[`50ch`, `80ch`]}
+      >
+        {children}
+      </Container>
+      <br />
+    </PageLayout>
+  );
+};
 
 export default CenteredLayout;
