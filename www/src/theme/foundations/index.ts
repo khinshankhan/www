be3870b58578b0a36855da62a98b7b28d46275e6
Khinshan Khan <khanshan501@gmail.com>
2022-06-06 17:24:17 -0400

    fill in missing theme gaps for spacing

--- www/src/theme/foundations/index.ts
+++ www/src/theme/foundations/index.ts
@@ -2,12 +2,14 @@ import { ThemeOverride } from "@chakra-ui/react";
 import breakpoints from "./breakpoints";
 import colors from "./colors";
 import semanticTokens from "./semantic-tokens";
+import space from "./space";
 import typography from "./typography";
 
 const foundations: ThemeOverride["foundations"] = {
   breakpoints,
   colors,
   semanticTokens,
+  space,
   ...typography,
 };
 
