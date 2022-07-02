5c64b541c7928e8c8bcf41302cbe7cba48a43820
Khinshan Khan <khanshan501@gmail.com>
2022-07-02 01:34:34 -0400

    update root 404 copy

--- www/src/pages/404.tsx
+++ www/src/pages/404.tsx
@@ -9,10 +9,11 @@ const NotFound = () => (
       <i>Sorry</i>, it seems you have stumbled into a page that doesn't exist (yet?). It's possible
       this page may come into existence later in the future... but for now, the way out is easy –
       you can try going back to the previous page or easily navigate over to the {` `}
-      <InternalLink href="/">home page</InternalLink>. If you really feel like something is missing
-      that should be here, {` `}
-      <InternalLink href="/contact">contact me</InternalLink> and I'll assist to the best of my
-      capacity 😊
+      <InternalLink href="/">home page</InternalLink>.
+    </Text>
+    <Text>
+      If you really feel like something is missing that should be here, feel free to {` `}
+      <InternalLink href="/contact">contact me</InternalLink> 😊
     </Text>
   </Layout>
 );
