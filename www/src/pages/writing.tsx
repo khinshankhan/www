def39de85ffbc61bf05c6007ddf453ce6cb59cd1
Khinshan Khan <khanshan501@gmail.com>
2022-07-06 23:25:48 -0400

    poc ability to add chakra components to display mdx

--- www/src/pages/writing.tsx
+++ www/src/pages/writing.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from "react";
+import { MDXProvider } from "@mdx-js/react";
 import { PageProps, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import Headings from "src/components/common/Heading/Headings";
 import { PageLayout as Layout } from "src/components/layouts";
 import { MdxQuery, WritingPageNodes } from "src/types/queries";
+
+const components = { ...Headings };
 
 const Index: FC<PageProps<MdxQuery<WritingPageNodes>>> = ({
   data: {
@@ -10,27 +14,29 @@ const Index: FC<PageProps<MdxQuery<WritingPageNodes>>> = ({
   },
 }) => (
   <Layout title="Writing" taglines={[`My thoughts and ideas`]}>
-    {nodes.map((n) => {
-      const {
-        body,
-        frontmatter: { title, spoiler },
-        id,
-      } = n;
+    <MDXProvider components={components}>
+      {nodes.map((n) => {
+        const {
+          body,
+          frontmatter: { title, spoiler },
+          id,
+        } = n;
 
-      /* const { title, spoiler } = n.frontmatter; */
+        /* const { title, spoiler } = n.frontmatter; */
 
-      const subtitle = spoiler ?? `A little surprise reading 😊`;
+        const subtitle = spoiler ?? `A little surprise reading 😊`;
 
-      return (
-        <div key={id}>
-          <p>{title}</p>
-          <p style={{ marginLeft: 10 }}>{subtitle}</p>
-          <div style={{ marginLeft: 50 }}>
-            <MDXRenderer>{body}</MDXRenderer>
+        return (
+          <div key={id}>
+            <p>{title}</p>
+            <p style={{ marginLeft: 10 }}>{subtitle}</p>
+            <div style={{ marginLeft: 50 }}>
+              <MDXRenderer>{body}</MDXRenderer>
+            </div>
           </div>
-        </div>
-      );
-    })}
+        );
+      })}
+    </MDXProvider>
   </Layout>
 );
 
