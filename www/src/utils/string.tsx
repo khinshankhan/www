6fa7fb8b629697f9204410b21a5ffe07d6241751
Khinshan Khan <khanshan501@gmail.com>
2022-07-07 19:23:48 -0400

    refine slug creation logic

--- www/src/utils/string.tsx
+++ www/src/utils/string.tsx
@@ -7,9 +7,9 @@ export const capitalize = (s: string, splitter = ` `, rejoin = ` `) =>
   s.toLowerCase().split(splitter).map(upperFirst).join(rejoin);
 
 type SlugifySource =
-  | { slug: undefined; title: undefined }
-  | { slug: string; title?: string }
-  | { slug: undefined; title: string };
+  | { slug: undefined; filePath: undefined }
+  | { slug: string; filePath?: string }
+  | { slug: undefined; filePath: string };
 
 /**
  * Creates a slug out of incoming source
@@ -24,12 +24,20 @@ export const slugify = (
     decamelize: false,
   }
 ): string => {
-  if (!source.slug && !source.title) {
+  if (!source.slug && !source.filePath) {
     return `/${sindresorhusSlugify(prefix, slugifyOptions)}`;
   }
 
-  const slug = source.slug ?? sindresorhusSlugify(source.title, slugifyOptions);
-  const p = sindresorhusSlugify(prefix, slugifyOptions);
+  let slug = ``;
+  if (source.slug === undefined) {
+    slug = sindresorhusSlugify(source.filePath, slugifyOptions);
+  } else {
+    slug = source.slug
+      .split(`/`)
+      .map((slugPart) => sindresorhusSlugify(slugPart, slugifyOptions))
+      .join(`/`);
+  }
 
+  const p = sindresorhusSlugify(prefix, slugifyOptions);
   return `/${p}/${slug}`.replace(singleSlashRegex, `/`);
 };
