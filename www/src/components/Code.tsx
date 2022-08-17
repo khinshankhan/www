2a5c04d8b5a191cd09a75acd2c3981ab6e8e8ceb
Khinshan Khan <shan@uptogood.dev>
2022-08-17 17:36:50 -0400

    highlight diff changes

--- www/src/components/Code.tsx
+++ www/src/components/Code.tsx
@@ -7,40 +7,64 @@ import type { FCC } from "src/types/react";
 interface ICodeProps {
   content: string;
   language: string;
-  diff: boolean;
   linesToHighlight: number[];
+  diff: boolean;
 }
 
 // NOTE: loosely based off https://prince.dev/prism-react-renderer except I have css written for gatsby-remark-prismjs,
 // so I have to write the rnederition like that
 // Basically copied whatever was outputted on commit 3e444df5c057195084dae0cc86a2544866abcb19
 
-const Code: FCC<ICodeProps> = ({ content, language, linesToHighlight }) => {
+const Code: FCC<ICodeProps> = ({ content, language, linesToHighlight, diff }) => {
   const code = content.trimEnd();
 
   const shouldHighlightLine = (i: number) => linesToHighlight.includes(i);
 
+  const languageClass = `language-${language}`;
+
   return (
     <Highlight {...defaultProps} code={code} language={language as Language}>
-      {({ tokens, getLineProps, getTokenProps }) => {
-        console.log({ tokens, getLineProps, getTokenProps });
-        return (
-          <Box as="code" className={`language-${language}`}>
-            {tokens.map((line, i) => {
-              const hlClassname = shouldHighlightLine(i + 1) ? `gatsby-highlight-code-line` : ``;
+      {({ tokens, getTokenProps }) => (
+        <Box as="code" className={languageClass}>
+          {tokens.map((line, i) => {
+            const hlClassName = shouldHighlightLine(i + 1) ? `gatsby-highlight-code-line` : ``;
 
-              return (
-                <div className={[hlClassname].join(` `)}>
-                  {line.map((token, key) => {
-                    const tokenProps = { ...getTokenProps({ token, key }), style: {} };
-                    return <span key={tokenProps.children} {...tokenProps} />;
-                  })}
-                </div>
-              );
-            })}
-          </Box>
-        );
-      }}
+            const cleanedLine =
+              line[0].content === `` && line[0].types[0] === `plain` ? line.slice(1) : line;
+
+            let diffClassName = ``;
+            if (diff) {
+              if (cleanedLine[0].content === `+`) {
+                diffClassName = `inserted-sign inserted`;
+              } else if (cleanedLine[0].content === `-`) {
+                diffClassName = `deleted-sign deleted`;
+              } else {
+                diffClassName = `unchanged`;
+              }
+            }
+
+            return (
+              <span className={[hlClassName, diffClassName, languageClass].join(` `)}>
+                {cleanedLine.map((token, key) => {
+                  const tokenProps = { ...getTokenProps({ token, key }), style: {} };
+                  let tokenClassName = tokenProps.className;
+                  if (diff) {
+                    if (token.content === `+`) {
+                      tokenClassName = `token prefix inserted`;
+                    } else if (token.content === `-`) {
+                      tokenClassName = `token prefix deleted`;
+                    }
+                  }
+
+                  return (
+                    <span key={tokenProps.children} {...tokenProps} className={tokenClassName} />
+                  );
+                })}
+              </span>
+            );
+          })}
+        </Box>
+      )}
     </Highlight>
   );
 };
