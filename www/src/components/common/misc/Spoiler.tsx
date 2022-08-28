ea5dcb6b6fe44efa6aec3b9afe0f0a3633dee50a
Khinshan Khan <shan@uptogood.dev>
2022-08-27 20:00:59 -0400

    make spoiler a controlled component

--- www/src/components/common/misc/Spoiler.tsx
+++ www/src/components/common/misc/Spoiler.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import type { FC } from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Fade, Icon, IconButton } from "@chakra-ui/react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import type { FCC } from "src/types/react";
 
 interface ISpoilerProps {
   text: string;
@@ -8,28 +9,48 @@ interface ISpoilerProps {
 
 // Inspired by: https://stackoverflow.com/a/41712116
 // TODO: circle back for a11y and better theming
-export const Spoiler: FC<ISpoilerProps> = ({ text }) => (
-  <Box
-    as="span"
-    cursor="default"
-    marginBottom="10px"
-    padding="10px"
-    backgroundColor="codeBg"
-    borderLeft="0.25em solid #baadb3"
-    tabIndex={0}
-    sx={{
-      "::after": {
-        content: `"${text}"`,
-        color: `transparent`,
-        padding: `0 0.5em`,
-      },
-      "&:focus::after, &:hover::after": {
-        cursor: `auto`,
-        color: `white`,
-        transition: `color .5s ease-in-out`,
-      },
-    }}
-  />
-);
+export const Spoiler: FCC<ISpoilerProps> = ({ text, children }) => {
+  const [show, setShow] = useState(false);
+  const toggleShow = () => setShow((prev) => !prev);
+  const icon = show ? FaRegEye : FaRegEyeSlash;
+  const ariaLabel = show ? `hide spoiler (to the right)` : `show spoiler (to the right)`;
+
+  return (
+    <Box as="span">
+      <Box as="span" marginBottom="10px" padding="10px" backgroundColor="inlineCodeBg">
+        <IconButton
+          aria-label={ariaLabel}
+          colorScheme="transparent"
+          onClick={toggleShow}
+          icon={<Icon as={icon} color="bgContrast" />}
+        />
+      </Box>
+      <Box
+        as="span"
+        cursor="default"
+        backgroundColor="codeBg"
+        marginBottom="10px"
+        padding="10px"
+        color="white"
+        sx={{
+          "::after": {
+            content: show ? `""` : `"${text || children}"`,
+            color: `transparent`,
+            padding: `0 0.5em`,
+          },
+        }}
+      >
+        <Fade
+          in={show}
+          style={{
+            display: `inline-block`,
+          }}
+        >
+          {show && (children || text)}
+        </Fade>
+      </Box>
+    </Box>
+  );
+};
 
 export default Spoiler;
