3d891b6db1f16c76bca0b9dac4b66a3661b7870d
Khinshan Khan <shan@uptogood.dev>
2022-11-13 00:03:14 -0500

    simplify and solidify logo animation

--- components/toggles/Home.tsx
+++ components/toggles/Home.tsx
@@ -8,39 +8,41 @@ interface IHomeToggleProps extends ILogoProps {
   size?: number | string;
 }
 
-export const HomeToggle = ({ size = 50, ...props }: IHomeToggleProps) => (
-  <IconButton
-    as={Link}
-    href="/"
-    aria-label={`Navigate to homepage`}
-    variant="ghost"
-    icon={
-      <Logo
-        width={size}
-        height={size}
-        bgColor="transparent"
-        fgColor="chakra-body-text"
-        borderColor="transparent"
-        {...props}
-      />
-    }
-    chakraProps={{
-      border: 2.75,
-      borderStyle: `solid`,
-      borderColor: `chakra-body-text`,
-      borderRadius: 7,
-      _hover: {
-        ".fg": {
-          fill: `primary`,
-          transform: `translate(-50px,-50px)`,
-          path: {
-            transform: `scale(1.25)`,
-          },
-        },
-        borderColor: `primary`,
-      },
-    }}
-  />
-);
+export const HomeToggle = ({ size = 50, ...props }: IHomeToggleProps) => {
+  const atProps = {
+    ".fg": {
+      fill: `primary`,
+      transform: `translate(-50px,-50px) scale(1.25)`,
+    },
+    borderColor: `primary`,
+  };
+
+  return (
+    <IconButton
+      as={Link}
+      href="/"
+      aria-label={`Navigate to homepage`}
+      variant="ghost"
+      icon={
+        <Logo
+          width={size}
+          height={size}
+          bgColor="transparent"
+          fgColor="chakra-body-text"
+          borderColor="transparent"
+          {...props}
+        />
+      }
+      chakraProps={{
+        border: 2.75,
+        borderStyle: `solid`,
+        borderColor: `chakra-body-text`,
+        borderRadius: 7,
+        _hover: atProps,
+        _focus: atProps,
+      }}
+    />
+  );
+};
 
 export default HomeToggle;
