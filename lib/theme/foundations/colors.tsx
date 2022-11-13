2a35a6a0b52ce6cc9fe198589f612a29ef54608c
Khinshan Khan <shan@uptogood.dev>
2022-11-13 11:41:51 -0500

    surface colors and tokenization more explicitly

---
+++ lib/theme/foundations/colors.tsx
@@ -0,0 +1,12 @@
+import { theme as base } from "@chakra-ui/react";
+
+export const colorPalette = {
+  ...base.colors,
+};
+
+export const colors = {
+  ...colorPalette,
+  brand: base.colors.purple,
+};
+
+export default colors;
--- lib/theme/foundations/semantic-tokens.tsx
+++ lib/theme/foundations/semantic-tokens.tsx
@@ -1,8 +1,29 @@
 const semanticTokens = {
   colors: {
+    "chakra-body-text": {
+      default: `gray.800`,
+      _dark: `whiteAlpha.900`,
+    },
+    "chakra-body-bg": {
+      default: `gray.50`,
+      _dark: `gray.900`,
+    },
+    "chakra-border-color": {
+      default: `gray.200`,
+      _dark: `whiteAlpha.300`,
+    },
+    "chakra-subtle-bg": {
+      default: `gray.100`,
+      _dark: `gray.700`,
+    },
+    "chakra-placeholder-color": {
+      default: `gray.500`,
+      _dark: `whiteAlpha.400`,
+    },
+
     primary: {
-      default: `purple.600`,
-      _dark: `purple.300`,
+      default: `brand.600`,
+      _dark: `brand.300`,
     },
   },
 };
