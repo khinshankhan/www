b117dfd1e6f238dfd0aa401f3770d7c5aca53f86
Khinshan Khan <shan@uptogood.dev>
2022-11-13 10:42:51 -0500

    smoother initial color change transition

---
+++ lib/theme/styles.tsx
@@ -0,0 +1,11 @@
+import type { Styles } from "@chakra-ui/theme-tools";
+
+const styles: Styles = {
+  global: {
+    body: {
+      transition: `background-color 0.4s ease-in-out`,
+    },
+  },
+};
+
+export default styles;
