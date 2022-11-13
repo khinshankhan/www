dfb2a8831fb1b9b9dd81bfdd08bc2a3d570363c1
Khinshan Khan <shan@uptogood.dev>
2022-11-13 18:11:55 -0500

    data for footer navigation

--- lib/contentlayer/documents/config/Meta.tsx
+++ lib/contentlayer/documents/config/Meta.tsx
@@ -1,4 +1,38 @@
-import { defineDocumentType } from "contentlayer/source-files";
+import { defineDocumentType, defineNestedType } from "contentlayer/source-files";
+
+const FooterSectionItem = defineNestedType(() => ({
+  name: `FooterSectionItem`,
+  fields: {
+    title: { type: `string`, required: true },
+    link: { type: `string`, required: true },
+  },
+}));
+
+const FooterSection = defineNestedType(() => ({
+  name: `FooterSection`,
+  fields: {
+    title: { type: `string`, required: true },
+    items: { type: `list`, of: FooterSectionItem, required: true },
+  },
+}));
+
+const FooterSocialItem = defineNestedType(() => ({
+  name: `FooterSocialItem`,
+  fields: {
+    // TODO: move socials enum options out
+    // TODO: add more socials here
+    title: { type: `enum`, options: [`github`, `linkedin`], required: true },
+    link: { type: `string`, required: true },
+  },
+}));
+
+const Footer = defineNestedType(() => ({
+  name: `Footer`,
+  fields: {
+    sections: { type: `list`, of: FooterSection, required: true },
+    socials: { type: `list`, of: FooterSocialItem, required: true },
+  },
+}));
 
 export const MetaConfig = defineDocumentType(() => ({
   name: `MetaConfig`,
@@ -15,6 +49,12 @@ export const MetaConfig = defineDocumentType(() => ({
       type: `number`,
       description: `Year for site's copyright`,
       required: true,
+    },
+
+    footer: {
+      type: `nested`,
+      of: Footer,
+      default: null,
     },
   },
   extensions: {},
