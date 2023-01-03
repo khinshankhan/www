6d2aa10b9be5673666344f9e90096bdd4a7b7a52
Khinshan Khan <shan@uptogood.dev>
2023-01-02 20:52:19 -0500

    add remark gfm

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,7 +1,15 @@
 import { makeSource } from "contentlayer/source-files";
 import * as documentTypes from "./lib/contentlayer/documents";
+import remarkGfm from "remark-gfm"
 
 export default makeSource({
   contentDirPath: `data`,
   documentTypes,
+  mdx: {
+    esbuildOptions(options) {
+      options.target = "esnext"
+      return options
+    },
+    remarkPlugins: [[remarkGfm]],
+  },
 });
