a4212c04a9ed9e46033f545dfe70d613692b4c76
Khinshan Khan <shan@uptogood.dev>
2023-01-04 21:58:06 -0500

    use url webapi instead of document create elem, assume site url

--- lib/utils/url.tsx
+++ lib/utils/url.tsx
@@ -1,3 +1,4 @@
+const siteUrl = "https://khinshankhan.com";
 export const isRelative = (url: string) => url.startsWith("/") || url.startsWith("#");
 
 // NOTE: extension logic should be used with the path rather than the url itself
@@ -28,10 +29,9 @@ const COMMON_URL_EXTENSIONS = [
   `net`,
 ];
 
-type IsUrlFile = (url: string, base?: string | undefined) => boolean;
-export const isUrlFile: IsUrlFile = (url, baseProp = undefined) => {
-  const base = isRelative(url) && (baseProp ?? "https://khinshankhan.com");
-  const destination = new URL(url, base || undefined);
+export const isUrlFile = (url: string, base = siteUrl) => {
+  const baseUrl = isRelative(url) && base;
+  const destination = new URL(url, baseUrl || undefined);
   const { pathname } = destination;
 
   if (!hasExtension(pathname)) return false;
@@ -42,12 +42,11 @@ export const isUrlFile: IsUrlFile = (url, baseProp = undefined) => {
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy
-export const onSameOrigin = (originUrl: string, destinationUrl: string) => {
-  // lifted off of gatsby-plugin-catch-links
-  const destination = document.createElement(`a`);
-  destination.href = originUrl;
-  const origin = document.createElement(`a`);
-  origin.href = destinationUrl;
+export const onSameOrigin = (originUrl: string, destinationUrl: string, base = siteUrl) => {
+  const baseOriginUrl = isRelative(originUrl) && base;
+  const origin = new URL(originUrl, baseOriginUrl || undefined);
+  const baseDestinationUrl = isRelative(destinationUrl) && base;
+  const destination = new URL(destinationUrl, baseDestinationUrl || undefined);
 
   return (
     origin.protocol === destination.protocol &&
@@ -61,13 +60,20 @@ interface IMatchLinkProps {
   link2: string;
   fullPath: boolean;
   excludeParams: boolean;
+  base?: string;
 }
 // this will be called after checking same origin, so we can assume as much
-export const matchLink = ({ link1, link2, fullPath, excludeParams }: IMatchLinkProps) => {
-  const anchorA = document.createElement(`a`);
-  anchorA.href = link1;
-  const anchorB = document.createElement(`a`);
-  anchorB.href = link2;
+export const matchLink = ({
+  link1,
+  link2,
+  fullPath,
+  excludeParams,
+  base = siteUrl,
+}: IMatchLinkProps) => {
+  const baseAUrl = isRelative(link1) && base;
+  const anchorA = new URL(link1, baseAUrl || undefined);
+  const baseBUrl = isRelative(link2) && base;
+  const anchorB = new URL(link2, baseBUrl || undefined);
 
   if (!excludeParams && anchorA.search !== anchorB.search) return false;
 
