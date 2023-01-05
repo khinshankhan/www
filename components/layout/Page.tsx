c94cc8a5ae439fdc0acf9f824d23b90a2e36fcda
Khinshan Khan <shan@uptogood.dev>
2023-01-05 18:01:58 -0500

    remove Heading component since styles are applied directly to tags

--- components/layout/Page.tsx
+++ components/layout/Page.tsx
@@ -2,7 +2,7 @@ import type { FCC } from "types/react";
 import React, { Fragment } from "react";
 import { styled } from "lib/theme";
 import clsx from "clsx";
-import { Box, Flex, Heading } from "lib/theme/components";
+import { Box, Flex } from "lib/theme/components";
 import { Header, Footer } from "./shared";
 
 const ContentHeader = styled("header", {
@@ -52,7 +52,7 @@ export const PageLayout: FCC<IPageLayoutProps> = ({
 
         <Box as="main">
           <ContentHeader className={clsx("nav-bg", intersect && "intersect")}>
-            <Heading.h1 css={{ paddingBottom: "24px" }}>{title}</Heading.h1>
+            <h1 style={{ paddingBottom: "24px" }}>{title}</h1>
             <Box as="p" className="main-nav">
               {subtitle}
             </Box>
--- lib/theme/components/index.tsx
+++ lib/theme/components/index.tsx
@@ -1,6 +1,5 @@
 export * from "./Box";
 export * from "./Flex";
-export * from "./Heading";
 export * from "./Icon";
 export * from "./IconButton";
 export * from "./Text";
