519056791b2a2551c79c46d47b358b1e66e81864
Khinshan Khan <shan@uptogood.dev>
2023-01-21 11:46:55 -0500

    fix typo

--- lib/rehype/rehypeMarkExcerpt.tsx
+++ lib/rehype/rehypeMarkExcerpt.tsx
@@ -1,6 +1,6 @@
 import { visit, SKIP, EXIT } from "unist-util-visit";
 
-export default function remarkMarkExcerpt() {
+export default function rehypeMarkExcerpt() {
   return (tree: any) => {
     visit(tree, "element", (node) => {
       if (node.tagName != "p") return [SKIP];
