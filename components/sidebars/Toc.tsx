3ae9f82e47eb1950b5dd14bfd1d8cf3bb9741fe8
Khinshan Khan <shan@uptogood.dev>
2023-01-20 23:13:41 -0500

    define active toc hash styles

--- components/sidebars/Toc.tsx
+++ components/sidebars/Toc.tsx
@@ -1,5 +1,5 @@
 import type { FCC } from "types/react";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link, Flex } from "components/primitives";
 import { styled } from "lib/theme";
 
@@ -17,11 +17,40 @@ const Li = styled("li", {
   "&[data-level='6']": {
     paddingLeft: "70px",
   },
+
+  transition: "box-shadow 0.4s ease-in-out",
+  "&[data-active='true']": {
+    boxShadow: "inset 4px 0px 0px 0px $colors$linkActive"
+  }
 })
 
 export type HeadingInfo = { id: string; level: number; content: string };
 
-export const Toc: FCC<{ headings: HeadingInfo[] }> = ({ headings }) => (
+export const Toc: FCC<{ headings: HeadingInfo[] }> = ({ headings }) => {
+  const [active, setActive] = useState("")
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActive(entry.target.id)
+          }
+        })
+      },
+      { rootMargin: `0% 0% -80% 0%` }
+    )
+
+    const existingElements = headings.map(({id}) => document.getElementById(id)).filter(Boolean)
+
+    existingElements.forEach((el) => {
+      if (el) {
+        observer.observe(el)
+      }
+    })
+
+  }, [])
+
+  return(
   <>
     <h2
       className="h4"
@@ -33,7 +62,7 @@ export const Toc: FCC<{ headings: HeadingInfo[] }> = ({ headings }) => (
     </h2>
     <Flex as="ul" flexDirection="column">
       {headings.map(({ id, level, content }) => (
-        <Li key={id} data-level={level}>
+        <Li key={id} data-level={level} data-active={active === id}>
           <Link href={`#${id}`}>
             {content}
           </Link>
@@ -41,4 +70,4 @@ export const Toc: FCC<{ headings: HeadingInfo[] }> = ({ headings }) => (
       ))}
     </Flex>
   </>
-);
+);}
