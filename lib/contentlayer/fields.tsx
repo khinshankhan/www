8dfd0206d38d731b0fa9ecadc4beb1479540d19c
Khinshan Khan <shan@uptogood.dev>
2023-01-20 20:06:46 -0500

    extract toc info per mdx file

--- lib/contentlayer/fields.tsx
+++ lib/contentlayer/fields.tsx
@@ -1,5 +1,6 @@
 import type { ComputedFields, FieldDefs } from "contentlayer/source-files";
 import { chopOffWord } from "../utils/string";
+import Slugger from "github-slugger";
 
 interface IFieldsProps {
   subtitle: string;
@@ -54,6 +55,24 @@ export const getComputedFields = <T extends string>({
         doc.givenSlug ?? chopPrefix
           ? cleanPath(doc._raw.flattenedPath).slice(1)
           : doc._raw.flattenedPath,
+    },
+
+    headings: {
+      type: "json",
+      resolve: (doc) => {
+        // use same package as rehypeSlug so toc and sluggified headings match
+        // https://github.com/rehypejs/rehype-slug/blob/main/package.json#L36
+        const slugs = new Slugger();
+
+        const regexHeadings = /^(?<tag>#{1,6})[ ](?<content>[^\n]+)/gm;
+
+        if (!doc?.body?.raw) return [];
+        return [...(doc.body.raw as string).matchAll(regexHeadings)].map(([, tag, content]) => ({
+          level: tag.length,
+          content,
+          id: slugs.slug(content, false),
+        }));
+      },
     },
   };
 };
