08e08cfba5bfaa2fe847d17b814592365d486dbe
Khinshan Khan <shan@uptogood.dev>
2023-01-28 16:57:15 -0500

    normalize styles for content

--- lib/theme/styles/global/normalize.tsx
+++ lib/theme/styles/global/normalize.tsx
@@ -34,6 +34,45 @@ export const normalizeStyles = {
       },
     },
   },
+
+  // gap between elements
+  "#content :where(h2, h3, h4, h5, h6, p, ol, ul)": {
+    marginBottom: "20px",
+  },
+  "#content p+:where(ol, ul)": {
+    marginTop: "-20px",
+  },
+
+  // list styling
+  ":where(#content, .list-style) :where(ul, ol)": {
+    listStyle: "inside",
+  },
+  ":where(#content, .list-style) ol": {
+    listStyleType: "decimal",
+  },
+  ":where(#content, .list-style) ol li ol": {
+    listStyleType: "lower-alpha",
+  },
+  ":where(#content, .list-style) ol li ol li ol": {
+    listStyleType: "lower-greek",
+  },
+  ":where(#content, .list-style) ul": {
+    listStyleType: "disc",
+  },
+  ":where(#content, .list-style) ul li ul": {
+    listStyleType: "circle",
+  },
+  ":where(#content, .list-style) ul li ul li ul": {
+    listStyleType: "square",
+  },
+
+  // 2 nested should be good enough
+  ":where(#content, .list-style) :where(ul, ol) li :where(ul, ol)": {
+    paddingLeft: "40px",
+  },
+  ":where(#content, .list-style) :where(ul, ol) li :where(ul, ol) li :where(ul, ol)": {
+    paddingLeft: "40px",
+  },
 };
 
 export default normalizeStyles;
