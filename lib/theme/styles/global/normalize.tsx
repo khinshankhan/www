39b7f17c93575c1693fd2b4de4a259d74209f8f6
Khinshan Khan <shan@uptogood.dev>
2023-01-28 19:57:11 -0500

    sweet link underline animation

--- lib/theme/styles/global/normalize.tsx
+++ lib/theme/styles/global/normalize.tsx
@@ -3,34 +3,58 @@ import { selectMedia } from "lib/theme/selectors";
 
 export const normalizeStyles = {
   a: {
+    position: "relative",
     fontFamily: "inherit",
     lineHeight: "inherit",
     fontSize: "inherit",
     color: "$link",
-    textDecoration: "underline 0 transparent",
-    textUnderlineOffset: "0.4em",
-    transition: "text-decoration-color 300ms",
+    textDecoration: "none",
+    // link underline animation based off https://tobiasahlin.com/blog/css-trick-animating-link-underlines/
+    [selectMedia("before")]: {
+      content: "''",
+      position: "absolute",
+      display: "block",
+      width: "100%",
+      height: "1px",
+      bottom: 0,
+      left: 0,
+      backgroundColor: theme.colors.linkActive,
+      transform: "scaleX(0)",
+      transition: "transform 0.3s ease",
+    },
     [selectMedia("hover")]: {
       color: "$linkActive",
-      textDecorationColor: "$linkActive",
-      textUnderlineOffset: "0.4em",
-    },
-    "&.on": {
-      color: "$linkOn",
-      "&:hover": {
-        color: "$linkOn",
+      [selectMedia("before")]: {
+        transform: "scaleX(1)",
       },
     },
+
+    "&.on": {
+      color: "$linkOn",
+      [selectMedia("before")]: {
+        transform: "scaleX(1)",
+        backgroundColor: theme.colors.link,
+      },
+      [selectMedia("hover")]: {
+        color: "$linkOn",
+        [selectMedia("before")]: {
+          backgroundColor: theme.colors.linkActive,
+        },
+      },
+    },
+
     "&.anchor": {
-      "&:after": {
+      [selectMedia("after")]: {
         transition: "opacity 0.4s ease-in-out",
         content: theme.icons.linkBefore.toString(),
         opacity: 0,
         marginLeft: "0.125em",
         verticalAlign: "middle",
       },
-      "&:hover:after": {
-        opacity: 1,
+      [selectMedia("hover")]: {
+        [selectMedia("after")]: {
+          opacity: 1,
+        },
       },
     },
   },
