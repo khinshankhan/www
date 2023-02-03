4cb1d5c78aa5b3bdd578015e8aa89f58df87a109
Khinshan Khan <shan@uptogood.dev>
2023-02-02 19:33:44 -0500

    recreate emoji component

---
+++ components/emoji.tsx
@@ -0,0 +1,20 @@
+import { styled } from "lib/theme";
+import type { Props } from "react-emoji-render";
+import EmojioneV4 from "react-emoji-render";
+
+export const EmojiStyled = styled(EmojioneV4, {});
+
+interface IEmojiProps extends Props {}
+export const Emoji = (props: IEmojiProps) => (
+  <EmojiStyled
+    options={{
+      protocol: "https",
+      baseUrl: "//cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/",
+      ext: "png",
+      size: "",
+    }}
+    {...props}
+  />
+);
+
+export default Emoji;
