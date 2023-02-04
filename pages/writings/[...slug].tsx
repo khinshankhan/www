3036a60193830ca250f0c4361d0df3a5cddba866
Khinshan Khan <shan@uptogood.dev>
2023-02-03 22:31:23 -0500

    render writings

---
+++ pages/writings/[...slug].tsx
@@ -0,0 +1,56 @@
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
+import type { Computed } from "lib/contentlayer";
+import type { Writing } from "contentlayer/generated";
+import { allWritingArticles as pages } from "lib/contentlayer";
+import { useLiveReload, useMDXComponent } from "next-contentlayer/hooks";
+import { Prose } from "components/layouts";
+
+const prefix = `writings`;
+
+export const getStaticPaths = () => {
+  const paths = pages.map((p) => ({
+    // NOTE: remove first part of path since next will add it in based on location in pages
+    params: { slug: p.slug!.split(`/`).slice(1) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<{
+  page: Writing;
+}> = async ({ params }) => {
+  // unknown case
+  if (!params?.slug) {
+    return {
+      notFound: true,
+    };
+  }
+
+  let slug = !Array.isArray(params.slug) ? params.slug : params.slug.join(`/`);
+  // add back prefix to match slug
+  slug = `${prefix}/${slug}`;
+  const page = pages.find((doc) => doc!.slug === slug);
+
+  if (!page) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      page,
+    },
+  };
+};
+
+export default function PageView({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
+  useLiveReload();
+  const MDXContent = useMDXComponent(page?.body?.code || "");
+
+  const computed = page.computed as Computed;
+  return <Prose {...computed}>{MDXContent && <MDXContent components={{}} />}</Prose>;
+}
