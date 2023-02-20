feccb72d7a9da5cf72c7e239e1eb4c5d3e06f970
Khinshan Khan <shan@uptogood.dev>
2023-02-20 14:22:34 -0500

    fill out theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
+function hsla(variable) {
+  return `hsla(var(--${variable}), <alpha-value>)`
+}
+
+function hslaN({ variable, n, scale = 1 }) {
+  return Array.from({ length: n }, (_, index) => index + 1).reduce((stored, num) => {
+    const key = num * scale
+    return {
+      ...stored,
+      [key]: hsla(`${variable}${key}`),
+    }
+  }, {})
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,7 +24,32 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: hslaN({ variable: "gray", n: 12 }),
+        mauve: hslaN({ variable: "mauve", n: 12 }),
+        plum: hslaN({ variable: "plum", n: 12 }),
+        violet: hslaN({ variable: "violet", n: 12 }),
+        bluegray: hslaN({ variable: "bluegray", n: 12 }),
+
+        theme: {
+          bg: hsla("bg"),
+          contentBg: hsla("content-bg"),
+          placeholder: hsla("placeholder"),
+        },
+
+        logo: {
+          fg: hsla("logo-fg"),
+          bg: hsla("logo-bg"),
+          border: hsla("logo-border"),
+        },
+        link: {
+          base: hsla("link"),
+          active: hsla("link-active"),
+          on: hsla("link-on"),
+        },
+      },
+    },
   },
   plugins: [],
 }
