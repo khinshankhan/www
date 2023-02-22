47f6a7a1661fdc8f1ef570707ddc11eee9fb3ab3
Khinshan Khan <shan@uptogood.dev>
2023-02-22 12:59:05 -0500

    fix nextjs warning about meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 import { ThemeProvider } from "components/providers"
 import "styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
