a5032efa56061f6aec358cd5d29fc8aedcd17261
Khinshan Khan <shan@uptogood.dev>
2023-02-22 00:39:23 -0500

    add viewport meta tags for safari

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" href="/logo.svg" />
       </Head>
       <body>
