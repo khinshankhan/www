f9e42e1b350636f5fe50372132d6b6c272a2cc2b
Khinshan Khan <shan@uptogood.dev>
2023-02-22 00:35:50 -0500

    home icon used with temporary home toggle

---
+++ components/toggles/home.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React from "react"
+
+import { Logo, type ILogoProps } from "components/icons"
+
+interface IHomeToggleProps extends ILogoProps {
+  size?: number | string
+  scalable?: boolean
+}
+
+// TODO: build out link wrapper
+export function HomeToggle({
+  size: sizeProp = undefined,
+  scalable = true,
+  ...props
+}: IHomeToggleProps) {
+  const size = "50px"
+  return <Logo width={size} height={size} {...props} />
+}
--- components/toggles/index.tsx
+++ components/toggles/index.tsx
@@ -1,1 +1,2 @@
-export * from "./theme-toggle"
+export * from "./home"
+export * from "./theme"
