0304cc63863459489ba1e66229610fcb0d0ef72d
Khinshan Khan <shan@uptogood.dev>
2023-02-22 16:15:15 -0500

    hooks for media

---
+++ hooks/use-breakpoint.tsx
@@ -0,0 +1,17 @@
+import { screens, type Screens } from "lib/theme"
+import { useMediaQuery } from "./use-media"
+
+type Bp = keyof Screens
+export function useBreakpoint(bp: Bp, defaultValue: boolean = false) {
+  function constructMediaQuery(bp: keyof typeof screens) {
+    const size = screens[bp]
+    if (typeof size === "object" && size?.max) {
+      return `(max-width: ${size.max})`
+    }
+    return `(min-width: ${size})`
+  }
+
+  const mediaQuery = constructMediaQuery(bp)
+  const match = useMediaQuery(mediaQuery, defaultValue)
+  return match
+}
--- lib/theme/index.tsx
+++ lib/theme/index.tsx
@@ -1,4 +1,6 @@
 import config from "./config"
 
 export const screens = config.screens
+export type Screens = typeof screens
+
 export const zIndex = config.zIndex
