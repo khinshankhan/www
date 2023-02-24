9aa8ad1842648f72e4724b5416db019e027534f8
Khinshan Khan <shan@uptogood.dev>
2023-02-23 22:03:01 -0500

    fix forward ref for menu toggle

--- components/toggles/menu.tsx
+++ components/toggles/menu.tsx
@@ -1,22 +1,19 @@
 "use client"
 
-import React from "react"
+import React, { forwardRef, type HTMLProps } from "react"
 
 import { Hamburger, XMark } from "components/icons"
 import { IconButton } from "components/ui"
 
-export interface MenuToggleProps {
+type ButtonProps = HTMLProps<HTMLButtonElement>
+export interface MenuToggleProps extends ButtonProps {
+  type?: "button" | "submit" | "reset"
   className?: string
   isOpen?: boolean
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export function MenuToggle({
-  className = "",
-  isOpen,
-  onClick = () => {},
-  ...props
-}: MenuToggleProps) {
+export const MenuToggle = forwardRef<HTMLButtonElement, MenuToggleProps>((props, ref) => {
+  const { className = "", isOpen, type: buttonType, ...rest } = props
   const MenuIcon = isOpen ? XMark : Hamburger
   const action = isOpen ? "Close" : "Open"
 
@@ -24,11 +21,14 @@ export function MenuToggle({
     <IconButton
       className={className}
       aria-label={`${action} navigation menu`}
-      onClick={onClick}
       isRound={false}
-      {...props}
+      type={buttonType ?? "button"}
+      // this should work but ts is being annoying
+      // @ts-ignore
+      ref={ref}
+      {...rest}
     >
       <MenuIcon />
     </IconButton>
   )
-}
+})
--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -1,21 +1,15 @@
-import React, { type ReactNode } from "react"
+import React, { forwardRef, type HTMLProps } from "react"
 
 import { cx } from "lib/utils"
 
-interface IconButtonProps {
+type ButtonProps = HTMLProps<HTMLButtonElement>
+interface IconButtonProps extends ButtonProps {
+  type?: "button" | "submit" | "reset"
   isRound?: boolean
   className?: string
-  children: ReactNode
-  // TODO: use button props
-  [key: string]: any
 }
-
-export function IconButton({
-  className = "",
-  isRound = true,
-  children,
-  ...props
-}: IconButtonProps) {
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  const { className = "", isRound = true, children, type: buttonType, ...rest } = props
   return (
     <button
       className={cx(
@@ -23,9 +17,11 @@ export function IconButton({
         isRound ? "rounded-full" : "rounded-md",
         className
       )}
-      {...props}
+      type={buttonType ?? "button"}
+      ref={ref}
+      {...rest}
     >
       {children}
     </button>
   )
-}
+})
