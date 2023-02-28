4d90bab1e7008e293cc542f9395768e6bf419c37
Khinshan Khan <shan@uptogood.dev>
2023-02-27 19:59:04 -0500

    replace list bullet with chevron arrow and allow icon svg classes

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { cx } from "lib/utils"
+
 export interface ILogoProps {
   width?: number | string
   height?: number | string
@@ -40,15 +42,19 @@ export function Logo({
   )
 }
 
-export function Moon() {
+export interface RawIconProps {
+  className?: string
+}
+
+export function Moon({ className = "" }: RawIconProps) {
   return (
     <svg
+      className={cx("h-6 w-6", className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
     >
       <path
         strokeLinecap="round"
@@ -59,15 +65,15 @@ export function Moon() {
   )
 }
 
-export function Sun() {
+export function Sun({ className = "" }: RawIconProps) {
   return (
     <svg
+      className={cx("h-6 w-6", className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
     >
       <path
         strokeLinecap="round"
@@ -78,15 +84,15 @@ export function Sun() {
   )
 }
 
-export function Hamburger() {
+export function Hamburger({ className = "" }: RawIconProps) {
   return (
     <svg
+      className={cx("h-6 w-6", className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
     >
       <path
         strokeLinecap="round"
@@ -97,36 +103,32 @@ export function Hamburger() {
   )
 }
 
-export function XMark() {
+export function XMark({ className = "" }: RawIconProps) {
   return (
     <svg
+      className={cx("h-6 w-6", className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
   )
 }
 
-export function ListBullet() {
+export function ChevronDown({ className = "" }: RawIconProps) {
   return (
     <svg
+      className={cx("h-6 w-6", className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-      />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
     </svg>
   )
 }
