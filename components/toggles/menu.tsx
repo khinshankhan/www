9332446b77b9020d1fa03ef0b42a4bf1bac7fdf2
Khinshan Khan <shan@uptogood.dev>
2023-02-27 19:58:37 -0500

    simplify down menu icon

--- components/toggles/menu.tsx
+++ components/toggles/menu.tsx
@@ -2,37 +2,29 @@
 
 import React, { forwardRef, type HTMLProps } from "react"
 
-import { IconButton } from "components/ui"
+import { Icon, IconButton } from "components/ui"
 import { Hamburger, XMark } from "components/icons"
 
 type ButtonProps = HTMLProps<HTMLButtonElement>
 export interface MenuToggleProps extends ButtonProps {
+  isOpen: boolean
   type?: "button" | "submit" | "reset"
   className?: string
-  isOpen?: boolean
-  openIcon?: () => JSX.Element
-  closeIcon?: () => JSX.Element
 }
 
+// NOTE: is this just a one off element? maybe it should be moved closed to its usage of it is
 export const MenuToggle = forwardRef<HTMLButtonElement, MenuToggleProps>(function MenuToggle(
   props,
   ref
 ) {
-  const {
-    className = "",
-    isOpen,
-    type: buttonType,
-    openIcon = Hamburger,
-    closeIcon = XMark,
-    ...rest
-  } = props
-  const MenuIcon = isOpen ? closeIcon : openIcon
+  const { className = "", isOpen, type: buttonType, ...rest } = props
+  const MenuIcon = isOpen ? XMark : Hamburger
   const action = isOpen ? "Close" : "Open"
 
   return (
     <IconButton
       className={className}
-      aria-label={`${action} navigation menu`}
+      aria-label={`${action} navigation menu.`}
       isRound={false}
       type={buttonType ?? "button"}
       // this should work but ts is being annoying
@@ -40,7 +32,9 @@ export const MenuToggle = forwardRef<HTMLButtonElement, MenuToggleProps>(functio
       ref={ref}
       {...rest}
     >
-      <MenuIcon />
+      <Icon>
+        <MenuIcon />
+      </Icon>
     </IconButton>
   )
 })
