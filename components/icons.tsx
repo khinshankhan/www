37cd0f0debb3e93cc44e57ca10062ebf554442e7
Khinshan Khan <shan@uptogood.dev>
2023-03-06 14:40:32 -0500

    handle different types of links in the application

--- components/icons.tsx
+++ components/icons.tsx
@@ -153,3 +153,43 @@ export function ArrowUp({ className = "", ...props }: RawIconProps) {
     </svg>
   )
 }
+
+export function ArrowUpRight({ className = "", ...props }: RawIconProps) {
+  return (
+    <svg
+      className={cx("h-6 w-6", className)}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  )
+}
+
+export function ArrowDownTray({ className = "", ...props }: RawIconProps) {
+  return (
+    <svg
+      className={cx("h-6 w-6", className)}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+      />
+    </svg>
+  )
+}
--- components/ui/link.tsx
+++ components/ui/link.tsx
@@ -1,11 +1,68 @@
 import React, { type ReactNode } from "react"
 import NextLink, { type LinkProps as NextLinkProps } from "next/link"
 
+import * as url from "lib/utils/url"
+
+import { ArrowDownTray, ArrowUpRight } from "components/icons"
+
+function ExternalIcon() {
+  return (
+    <span className="align-super">
+      <ArrowUpRight className="inline h-4 w-4" />
+    </span>
+  )
+}
+
+function FileIcon() {
+  return (
+    <span className="align-sub">
+      <ArrowDownTray className="inline h-4 w-4" />
+    </span>
+  )
+}
+
 interface LinkProps extends NextLinkProps {
+  isInternal?: boolean
+  isExternal?: boolean
+  isFile?: boolean
   className?: string
   children: ReactNode
 }
 
-export function Link({ children, ...props }: LinkProps) {
-  return <NextLink {...props}>{children}</NextLink>
+export function Link({
+  href,
+  isInternal: isInternalProp = undefined,
+  isExternal: isExternalProp = undefined,
+  isFile: isFileProp = undefined,
+  children,
+  ...props
+}: LinkProps) {
+  const isExternal = (isInternalProp && !isInternalProp) ?? isExternalProp ?? !url.isRelative(href)
+  const isFile = isFileProp ?? url.isUrlFile(href)
+
+  const VisualIcon = isExternal ? <ExternalIcon /> : isFile ? <FileIcon /> : null
+
+  // if href is a url obj it's a local link with state (probably), and / is totally local
+  if (typeof href !== "string" || href.startsWith("/")) {
+    return (
+      <NextLink href={href} {...props}>
+        {children}
+        {VisualIcon}
+      </NextLink>
+    )
+  }
+
+  // # means same page, but use normal anchor tag for smoother scroll
+  // ideally the icon will be the anchor icon, however it'll have to be placed in the mdx logic to avoid adding the icon for toc
+  if (href.startsWith("#")) {
+    return <a {...props}>{children}</a>
+  }
+
+  // external link
+  return (
+    <a target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+      {VisualIcon}
+    </a>
+  )
 }
