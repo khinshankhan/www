980c0dd536dc78276428ba4d7316b2ba3d9c618b
Khinshan Khan <shan@uptogood.dev>
2023-05-10 23:24:21 -0400

    init new next project with app dir

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import React, { type ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
