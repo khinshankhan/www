158380ef6f44f0f37d5de65baea01838cc55f1d4
Khinshan Khan <shan@uptogood.dev>
2023-05-21 16:50:36 -0400

    fix conflicting star exports warning

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,5 +1,7 @@
 export * from "./button"
-export * from "./collapsible"
+/* NOTE: contains conflicting star exports for the names '$$typeof', '__esModule' with './tooltip' */
+export { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./collapsible"
 export * from "./link"
-export * from "./tooltip"
+/* NOTE: contains conflicting star exports for the names '$$typeof', '__esModule' with './collapsible' */
+export { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip"
 export * from "./typography"
