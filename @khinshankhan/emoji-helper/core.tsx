cab7dccfab4e993224948aedf455c7edcb975e70
Khinshan Khan <shan@uptogood.dev>
2023-05-21 22:01:34 -0400

    emoji logic

---
+++ @khinshankhan/emoji-helper/core.tsx
@@ -0,0 +1,58 @@
+// NOTE: someday this might become a library...
+
+export interface EmojiInfoTemplate {
+  names: readonly string[]
+  url: string
+  char?: string
+  // not sure if this is useful but it might be interesting
+  description?: string
+}
+
+export class DuplicateEmojiException extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = "DuplicateEmojiException"
+  }
+}
+
+export class InvalidEmojiException extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = "InvalidEmojiException"
+  }
+}
+
+export function createEmojiLookup<
+  EmojiPack extends readonly EmojiInfoTemplate[],
+  Key extends `:${EmojiPack[number]["names"][number]}:`
+>(emojiPacks: EmojiPack[], overwrite = false, merge = false, supressOverwriteWarnings = false) {
+  const lookup = new Map<Key, { alt: Key; url: string; char?: string | undefined }>()
+
+  emojiPacks.forEach((emojiPack) => {
+    emojiPack.forEach((rawEmojiInfo) => {
+      rawEmojiInfo.names.forEach((name) => {
+        const key = `:${name}:` as Key
+
+        if (lookup.has(key)) {
+          if (!overwrite) {
+            throw new DuplicateEmojiException(`Lookup already has ${key}`)
+          }
+          if (!supressOverwriteWarnings) {
+            console.warn(`Lookup already has ${key}`)
+          }
+        }
+
+        const prevInfo = lookup.get(key) ?? {}
+        const newInfo = {
+          alt: key,
+          url: rawEmojiInfo.url,
+          char: rawEmojiInfo?.char,
+        }
+
+        lookup.set(key, !merge ? newInfo : { ...prevInfo, ...newInfo })
+      })
+    })
+  })
+
+  return lookup
+}
