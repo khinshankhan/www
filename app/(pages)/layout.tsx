1c4aa45e6dede92e97e34156bac3f49798ce24d8
Khinshan Khan <shan@uptogood.dev>
2023-05-29 13:12:17 -0400

    update server invite info

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -8,12 +8,12 @@ export default function PagesLayout({ children }: { children: ReactNode }) {
 
       <div className="bg-theme-primary pb-8">
         <div className="page-container">
-          <div className="mt-4 text-center">
+          <div className="mx-auto mt-4 max-w-[80ch] text-center">
             {`Hey there, congrats on making it all the way down here. Do you want chat about the
             content you just read? Maybe about work I'm planning or you'd like to see? Wondering
-            where's the disqus or utterances chat? I'm experimenting with building a community, so
+            where's the chat? I'm experimenting with building a community, so
             please`}{" "}
-            <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">join my discord server</Link>.
+            <Link href="https://discord.gg/J8Zh6u4jB4">join my discord server</Link>.
           </div>
         </div>
       </div>
