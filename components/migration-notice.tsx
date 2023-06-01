d790eb5faae22be63d350e0c427e50a3c3882d3d
Khinshan Khan <shan@uptogood.dev>
2023-06-01 07:47:11 -0400

    create callout with status variants

--- components/migration-notice.tsx
+++ components/migration-notice.tsx
@@ -1,29 +1,27 @@
-import React, { type ReactNode } from "react"
-import { cn } from "@/lib/utils"
-import { Icon } from "@/components/ui"
+import React from "react"
+import { Link } from "@/components/ui"
+import { Callout, CalloutProps } from "@/components/callout"
 import Emoji from "@/components/emoji"
-import { InformationCircle } from "@/components/icons"
 
-interface MigrationNoticeProps {
-  className?: string
-  children?: ReactNode
-}
-export default function MigrationNotice({ className = "", children }: MigrationNoticeProps) {
+export default function MigrationNotice({ children, ...props }: CalloutProps) {
   return (
-    <div className={cn("align-center flex flex-row justify-center", className)}>
-      <div className="max-w-[60ch] border p-4">
-        <Icon>
-          <InformationCircle className="stroke-sky-8" />
-        </Icon>{" "}
-        {children}
-        {!children && (
-          <>
-            <span>
-              {`I'm in the process of rewriting my website. It'll take some time, so this is a placeholder until then! Soon`}{" "}
-            </span>
-            <Emoji name=":construction:" />
-          </>
-        )}
+    <div className="align-center flex w-full flex-row justify-center text-left">
+      <div className="w-[60ch]">
+        <Callout variant="info" heading="Work in Progress" {...props}>
+          {children}
+          {!children && (
+            <>
+              <span>
+                {`I'm in the process of rewriting my website. You can view the progress on`}{" "}
+                <Link href="https://github.com/khinshankhan/anchorage">GitHub</Link>
+                {`It'll take some time, so this is a placeholder until then! Soon`}{" "}
+              </span>
+              <sup>
+                <Emoji name=":tm:" />
+              </sup>
+            </>
+          )}
+        </Callout>
       </div>
     </div>
   )
