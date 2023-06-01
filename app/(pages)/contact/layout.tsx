de06ea8788481b93af8ef0dae0f44ac54ea84a6a
Khinshan Khan <shan@uptogood.dev>
2023-06-01 08:09:00 -0400

    add playful emoji

--- app/(pages)/contact/layout.tsx
+++ app/(pages)/contact/layout.tsx
@@ -1,11 +1,16 @@
 import React, { type ReactNode } from "react"
+import Emoji from "@/components/emoji"
 import { PageSkeletonLayout } from "@/components/layouts"
 
 export default function PagesLayout({ children }: { children: ReactNode }) {
   return (
     <PageSkeletonLayout
       title="Contact"
-      subtitle="Getting in touch. Boop."
+      subtitle={
+        <>
+          <span>{`Getting in touch. Boop`}</span> <Emoji name=":point_up_2:" />
+        </>
+      }
       path="/app/(pages)/contact/page.tsx"
     >
       {children}
--- app/(pages)/projects/layout.tsx
+++ app/(pages)/projects/layout.tsx
@@ -1,11 +1,16 @@
 import React, { type ReactNode } from "react"
+import Emoji from "@/components/emoji"
 import { PageSkeletonLayout } from "@/components/layouts"
 
 export default function PagesLayout({ children }: { children: ReactNode }) {
   return (
     <PageSkeletonLayout
       title="Projects"
-      subtitle="Some work. Almost a portfolio."
+      subtitle={
+        <>
+          <span>{`Some work. Almost a portfolio`}</span> <Emoji name=":dancer:" />
+        </>
+      }
       path="/app/(pages)/projects/page.tsx"
     >
       {children}
