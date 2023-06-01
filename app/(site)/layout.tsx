a832cf4e1fef30edd2099bab2cfab63a81e7443e
Khinshan Khan <shan@uptogood.dev>
2023-06-01 13:12:23 -0400

    create og image

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -21,6 +21,13 @@ export const metadata: Metadata = {
   icons: {
     shortcut: "/favicon.ico",
   },
+  images: [
+    {
+      url: "https://khinshankhan.com/og.png?v=1",
+      width: 1200,
+      height: 630,
+    },
+  ],
 }
 
 const headingFont = Montserrat({
