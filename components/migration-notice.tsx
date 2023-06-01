129ead4d5d218a20572008d58153adef0332b274
Khinshan Khan <shan@uptogood.dev>
2023-05-31 20:05:14 -0400

    fill out homepage

--- components/migration-notice.tsx
+++ components/migration-notice.tsx
@@ -1,11 +1,16 @@
 import React, { type ReactNode } from "react"
+import { cn } from "@/lib/utils"
 import { Icon } from "@/components/ui"
 import Emoji from "@/components/emoji"
 import { InformationCircle } from "@/components/icons"
 
-export default function MigrationNotice({ children }: { children?: ReactNode }) {
+interface MigrationNoticeProps {
+  className?: string
+  children?: ReactNode
+}
+export default function MigrationNotice({ className = "", children }: MigrationNoticeProps) {
   return (
-    <div className="align-center flex flex-row justify-center">
+    <div className={cn("align-center flex flex-row justify-center", className)}>
       <div className="max-w-[60ch] border p-4">
         <Icon>
           <InformationCircle className="stroke-sky-8" />
