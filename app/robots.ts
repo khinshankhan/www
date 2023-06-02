2970f6564fd32170e999d7f9975c49219c8740db
Khinshan Khan <shan@uptogood.dev>
2023-06-02 13:32:20 -0400

    robots file has to be at the root of app dir apparently

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private/", "/drafts/"],
+      },
+    ],
     sitemap: "https://khinshankhan.com/sitemap.xml",
+    host: "https://khinshankhan.com",
   }
 }
