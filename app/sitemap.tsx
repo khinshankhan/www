ba9c6e7a3e1435ba6b3eaa7e73c061aaf197df9e
Khinshan Khan <shan@uptogood.dev>
2023-06-03 10:24:52 -0400

    add content to sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,14 +1,15 @@
 import { MetadataRoute } from "next"
-import { allPages } from "contentlayer/generated"
+import { allPages, allWritings } from "contentlayer/generated"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   // NOTE: this list is manually maintained for any page that isn't generated via contentlayer
   const routes = ["", "/writings", "/projects", "/contact"].map((route) => ({
     url: `https://www.khinshankhan.com${route}`,
+    // TODO: wondering if this should be overengineered
     lastModified: new Date().toISOString().split("T")[0],
   }))
 
-  const generatedRoutes = [...allPages].map((page) => ({
+  const generatedRoutes = [...allPages, ...allWritings].map((page) => ({
     url: `https://www.khinshankhan.com/${page.slug}`,
     lastModified: page.tended,
   }))
