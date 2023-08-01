f2697a967cf3b9fc04faf37746f5daacd421aeff
Khinshan Khan <shan@uptogood.dev>
2023-07-31 21:28:38 -0400

    change up layout to show cover image for writing articles

--- app/(site)/(pages)/(content)/writings/[...slug]/layout.tsx
+++ app/(site)/(pages)/(content)/writings/[...slug]/layout.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactNode } from "react"
 import { notFound } from "next/navigation"
 import { Computed } from "@/lib/contentlayer/documents"
+import { Link, SmartImage } from "@/components/ui"
 import { PageSkeletonLayout, Toc } from "@/components/layouts"
 import { getPageFromParams, type PageProps } from "./utils"
 
@@ -16,6 +17,8 @@ export default async function PagesLayout({ children, params }: PageLayoutProps)
   }
 
   const computed = page.computed as Computed
+  const cover = computed.frontmatter.cover
+  const tags = computed.tags
 
   return (
     <PageSkeletonLayout
@@ -24,6 +27,28 @@ export default async function PagesLayout({ children, params }: PageLayoutProps)
       path={`/data/${page._raw.sourceFilePath}`}
       sidebar={<Toc headings={computed.headings} />}
       className="normalize"
+      before={
+        cover && <SmartImage src={cover.img} alt={cover.alt} className="aspect-video md:!w-[75%]" />
+      }
+      after={
+        tags?.length !== 0 && (
+          <>
+            <div className="flex flex-row items-center justify-center">
+              <div className="md:w-[70%]">
+                <hr />
+              </div>
+            </div>
+
+            <div className="flex flex-row items-center justify-center gap-2">
+              {tags.map((e) => (
+                <Link key={e} href="/">
+                  {e}
+                </Link>
+              ))}
+            </div>
+          </>
+        )
+      }
     >
       {children}
     </PageSkeletonLayout>
--- components/layouts/page-skeleton.tsx
+++ components/layouts/page-skeleton.tsx
@@ -1,27 +1,44 @@
 import React, { type ReactNode } from "react"
 import { cn } from "@/lib/utils"
-import { Link, typographyVariants } from "@/components/ui"
+import { Link, SmartImage, typographyVariants } from "@/components/ui"
 
 export interface WithSidebarProps {
   direction?: "left" | "right"
   sidebar?: ReactNode
+
+  before?: ReactNode
   children: ReactNode
+  after?: ReactNode
 }
 
-export function WithSidebar({ direction = "left", sidebar, children }: WithSidebarProps) {
+export function WithSidebar({
+  direction = "left",
+  sidebar,
+  before,
+  children,
+  after,
+}: WithSidebarProps) {
   return (
-    <div
-      className={cn(
-        "page-container flex-col gap-20 xl:flex",
-        direction === "left" ? "xl:flex-row xl:justify-start" : "xl:flex-row-reverse xl:justify-end"
-      )}
-    >
-      {sidebar && (
-        <aside className="top-6 mt-6 pt-0 sm:top-2 sm:pt-2 xl:sticky xl:min-w-[225px] xl:max-w-[225px] xl:self-start 2xl:min-w-[275px] 2xl:max-w-[275px]">
-          {sidebar}
-        </aside>
-      )}
-      {children}
+    <div className="page-container flex flex-col gap-10">
+      {before}
+
+      <div
+        className={cn(
+          "flex-col gap-20 xl:flex",
+          direction === "left"
+            ? "xl:flex-row xl:justify-start"
+            : "xl:flex-row-reverse xl:justify-end"
+        )}
+      >
+        {sidebar && (
+          <aside className="top-6 mt-6 pt-0 sm:top-2 sm:pt-2 xl:sticky xl:min-w-[225px] xl:max-w-[225px] xl:self-start 2xl:min-w-[275px] 2xl:max-w-[275px]">
+            {sidebar}
+          </aside>
+        )}
+        {children}
+      </div>
+
+      {after}
     </div>
   )
 }
@@ -42,6 +59,8 @@ export function PageSkeletonLayout({
   path: ghPath,
   children,
   className = "",
+  before,
+  after,
 }: PageSkeletonLayoutProps) {
   // the flex grow applies to the base layout's min-h flex div. this keeps any negative space between content to footer
   // (within the min-h) the content bg color, accounting for potentially shorter content
@@ -55,7 +74,7 @@ export function PageSkeletonLayout({
       </header>
 
       <div className="grow py-5">
-        <WithSidebar direction={direction} sidebar={sidebar}>
+        <WithSidebar direction={direction} sidebar={sidebar} before={before} after={after}>
           <article id="article" className={cn("my-6 flex flex-1 flex-col pt-0 sm:pt-2", className)}>
             {children}
           </article>
