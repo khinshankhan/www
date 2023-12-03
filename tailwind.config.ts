73599a18112e57f9dcc92f62517986010c949abd
Khinshan Khan <shan@uptogood.dev>
2023-12-02 21:52:29 -0500

    set up general layout sections

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      xss: "320px",
+      xs: "392px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    fontFamily: {
+      heading: ["var(--font-geist-sans)", ...defaultTheme.fontFamily.sans],
+      body: ["var(--font-geist-sans)", ...defaultTheme.fontFamily.sans],
+      mono: ["var(--font-geist-mono)", ...defaultTheme.fontFamily.mono],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
