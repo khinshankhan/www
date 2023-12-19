382b319cc120bf56e870085252a370f75094fa7c
Khinshan Khan <shan@uptogood.dev>
2023-12-18 22:26:54 -0500

    basic next project setup

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello there</h1>
+    </div>
+  );
+}
