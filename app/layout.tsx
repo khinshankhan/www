bf2ee868856b5d0209d32470a8d6ad78159dfc2a
Khinshan Khan <shan@uptogood.dev>
2023-12-22 21:51:41 -0500

    general layout of where things will go

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Divider } from "@/components/primitives/divider";
 
 export const metadata: Metadata = {};
 
@@ -10,7 +11,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col bg-white dark:bg-zinc-950 text-zinc-950 dark:text-white min-h-screen">
+        <div className="flex flex-col min-h-[87vh]">
+          <div className="min-h-[100px]">Header goes here</div>
+          <div className="grow flex flex-col">{children}</div>
+        </div>
+
+        <footer className="grow min-h-[120px] pt-4">
+          <Divider />
+          <span>Footer goes here</span>
+        </footer>
+      </body>
     </html>
   );
 }
