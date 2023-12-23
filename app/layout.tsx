a4af16e5216eb6d72a17913cf8b2667e31ea7cd5
Khinshan Khan <shan@uptogood.dev>
2023-12-22 22:35:19 -0500

    dabbling with sticky header

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col bg-white dark:bg-zinc-950 text-zinc-950 dark:text-white min-h-screen">
-        <div className="flex flex-col min-h-[87vh]">
-          <div className="min-h-[100px]">Header goes here</div>
-          <div className="grow flex flex-col">{children}</div>
+      <body className="flex flex-col bg-white dark:bg-zinc-950 text-zinc-950 dark:text-white min-h-screen relative">
+        <div className="flex flex-col min-h-[87vh] relative isolate">
+          <nav className="sticky top-0 bg-white dark:bg-zinc-950/[80%] backdrop-blur-md z-1 pb-1">
+            <div className="container mx-auto">
+              <p>Header goes here</p>
+            </div>
+          </nav>
+
+          <div className="grow flex flex-col container mx-auto relative z-0 pt-6">
+            {children}
+          </div>
         </div>
 
         <footer className="grow min-h-[120px] pt-4">
