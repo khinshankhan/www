faf160dbd9eefa40b8a43a400305c736b2db24ab
Khinshan Khan <shan@uptogood.dev>
2023-12-31 22:28:02 -0500

    able to get toc from mdx

---
+++ lib/toc.tsx
@@ -0,0 +1,8 @@
+import { remark } from "remark";
+import { remarkTocExport, type TocItem } from "./mdx-plugins/remark-toc";
+
+export async function getToc(content: string) {
+  const vfile = await remark().use(remarkTocExport).process(content);
+
+  return (vfile?.data?.toc || []) as TocItem[];
+}
