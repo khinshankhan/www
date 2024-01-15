f9bf92442784f9736b89bcc7ab0c19f993464a14
Khinshan Khan <shan@uptogood.dev>
2024-01-15 15:46:43 -0500

    listing content

--- app/(content)/writings/page.tsx
+++ app/(content)/writings/page.tsx
@@ -1,13 +1,24 @@
-import { ContentPageLayout } from "@/components/layouts/content";
+import { getAllContentData } from "@/lib/content"
+import { PageLayout } from "@/components/layouts/page"
 
-export default async function Page() {
+export default function Page() {
+  // TODO: filter out 'root' content like about, contact, etc
+  const articles = getAllContentData()
+
   return (
-    <ContentPageLayout
-      title="Writings"
-      subtitle="My thoughts and ideas"
-      filePath="app/(content)/writings/page.tsx"
-    >
-      <main className="">Content goes here</main>
-    </ContentPageLayout>
-  );
+    <PageLayout title="Writings" subtitle="My thoughts and ideas">
+      <div className="text-center">TODO: pageo</div>
+
+      <ul id="content" className="space-y-4">
+        {articles.map((article) => (
+          <li key={article.slug}>
+            <a href={`/${article.slug}`}>
+              <p>{article.frontmatter.title}</p>
+              <p className="text-zinc-500">{article.frontmatter.subtitle}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </PageLayout>
+  )
 }
