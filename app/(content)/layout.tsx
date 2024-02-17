9f411a1444851511d9b27a7293e48f6071c554c4
Khinshan Khan <shan@uptogood.dev>
2024-02-16 21:53:40 -0500

    tabula rasa

--- app/(content)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function ContentLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex grow flex-col bg-secondary-background">{children}</main>
-}
--- app/(hero)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function HeroLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="grow bg-primary-background" id="article">
-      {children}
-    </main>
-  )
-}
