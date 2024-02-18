d882d2eb4b1a7aa7a0788054e19f2fc662d65423
Khinshan Khan <shan@uptogood.dev>
2024-02-18 09:48:33 -0500

    saner defaults

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
 
-export default nextConfig;
+/**
+ * @typedef {import('next').NextConfig} NextConfig
+ */
+
+/** @type {NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  trailingSlash: true,
+};
+
+/**
+ * Composes the configuration with a series of functions.
+ * @param {NextConfig} initial The initial configuration.
+ * @param {((config: NextConfig) => NextConfig)[]} fns The array of functions to compose.
+ * @returns {NextConfig} The composed configuration.
+ */
+const compose = (initial, fns) => fns.reduce((acc, fn) => fn(acc), initial);
+
+// Using ESM syntax for exporting the configuration.
+export default () => {
+  const plugins = [];
+  const config = compose(nextConfig, plugins);
+
+  return config;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./content/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      heading: ["var(--font-heading)", ...defaultTheme.fontFamily.sans],
+      body: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
+      mono: ["var(--font-mono)", ...defaultTheme.fontFamily.mono],
     },
   },
   plugins: [],
