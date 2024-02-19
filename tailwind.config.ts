149e2e223e4ae351a9e7b2c2c26ebc1f4a75c98a
Khinshan Khan <shan@uptogood.dev>
2024-02-19 03:51:26 -0500

    use new brakpoints to decide container sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,21 @@ const config: Config = {
     "./content/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xss: "320px",
+      xs: "392px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1325px",
+      "2xl": "1536px",
+
+      // usually I think in terms of this for design
+      // isMobile to isDesktop should have no gap ergo
+      // -1 so it doesn't encompass 768
+      isMobile: { max: "767px" },
+      isDesktop: "768px",
+    },
     fontFamily: {
       heading: ["var(--font-heading)", ...defaultTheme.fontFamily.sans],
       body: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
