eb51c11db8a0d949eb6339b14eec34761ed05bc4
Khinshan Khan <shan@uptogood.dev>
2024-02-19 21:59:19 -0500

    introduce writings page to see outcome

---
+++ app/writings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Lorem } from "@/components/lorem"
+
+export default function Writings() {
+  return (
+    <div className="normalize">
+      <p>henlo</p>
+      <Lorem />
+    </div>
+  )
+}
