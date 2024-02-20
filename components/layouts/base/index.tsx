fd4e5fa8f28ebc8f8e41d527576a40dd307c0020
Khinshan Khan <shan@uptogood.dev>
2024-02-19 21:40:21 -0500

    create base layout wireframe

---
+++ components/layouts/base/index.tsx
@@ -0,0 +1,58 @@
+import React, { type ReactNode } from "react"
+import { cn } from "@/lib/utils"
+import { typographyVariants } from "@/components/primitives/typography"
+
+export function Header() {
+  return (
+    <header role="navigation" className="sticky top-0 z-banner bg-red-200">
+      <nav className="page-container flex w-full flex-row items-center justify-between">
+        {/* lhs on all views */}
+        <div>K</div>
+
+        {/* rhs on desktop view */}
+        <div className={cn(typographyVariants({ variant: "nav" }), "hide-mobile flex flex-row")}>
+          <div>Links</div>
+          <div>Settings</div>
+        </div>
+
+        {/* rhs on mobile view */}
+        <div className={cn(typographyVariants({ variant: "nav" }), "hide-desktop flex flex-row")}>
+          <div>menu</div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export const info = {
+  fullname: "Khinshan Khan",
+  startYear: 2017,
+}
+
+export function Footer() {
+  return (
+    <footer>
+      <p className="text-center">
+        &copy; {info.startYear}+, {info.fullname}. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+interface BaseLayoutProps {
+  children: ReactNode
+}
+
+export function BaseLayout({ children }: BaseLayoutProps) {
+  return (
+    <>
+      <div className="relative z-0 flex min-h-[87vh] flex-col xs:min-h-[96vh]">
+        <Header />
+        <main className="flex grow flex-col">{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default BaseLayout
