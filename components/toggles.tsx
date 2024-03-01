67c841a82e44618993b06d85f6a257ed89ef2f32
Khinshan Khan <shan@uptogood.dev>
2024-02-29 23:54:44 -0500

    use dropdown menu for theme toggle

--- components/toggles.tsx
+++ components/toggles.tsx
@@ -1,10 +1,16 @@
 "use client"
 
-import React, { useCallback } from "react"
+import React from "react"
 import { useTheme } from "next-themes"
 import { cn } from "@/lib/utils"
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { Logo, type ILogoProps } from "@/components/icons"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/primitives/dropdown-menu"
 import { Link } from "@/components/primitives/link"
 
 interface IHomeToggleProps extends ILogoProps {
@@ -26,16 +32,28 @@ export function HomeToggle({
 }
 
 export function ModeToggle() {
-  const { setTheme, resolvedTheme } = useTheme()
-
-  const onToggle = useCallback(() => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark")
-  }, [setTheme, resolvedTheme])
+  const { setTheme, theme } = useTheme()
 
   return (
-    <button onClick={onToggle}>
-      <SunIcon className="block size-[1.2rem] dark:hidden" />
-      <MoonIcon className="hidden size-[1.2rem] dark:block" />
-    </button>
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <button>
+          <SunIcon className="block size-[1.2rem] dark:hidden" />
+          <MoonIcon className="hidden size-[1.2rem] dark:block" />
+          <span className="sr-only">Toggle theme</span>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem disabled={theme === "light"} onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled={theme === "dark"} onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled={theme === "system"} onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
