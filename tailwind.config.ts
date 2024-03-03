b7818044a2208ecd8a465456dedec99a99c21806
Khinshan Khan <shan@uptogood.dev>
2024-03-03 01:36:37 -0500

    simple wave animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -109,6 +109,24 @@ const config: Config = {
         "link-show": "100% 0.05em",
         full: "100%",
       },
+      keyframes: {
+        /* https://jarv.is/notes/css-waving-hand-emoji/ */
+        waving: {
+          "0%": { transform: "rotate( 0.0deg)" },
+          "10%": {
+            transform: "rotate(14.0deg)",
+          } /* The following five values can be played with to make the waving more or less extreme */,
+          "20%": { transform: "rotate(-8.0deg)" },
+          "30%": { transform: "rotate(14.0deg)" },
+          "40%": { transform: "rotate(-4.0deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate( 0.0deg)" } /* Reset for the last half to pause */,
+          "100%": { transform: "rotate( 0.0deg)" },
+        },
+      },
+      animation: {
+        wave: "waving 2.5s infinite",
+      },
     },
   },
   plugins: [],
