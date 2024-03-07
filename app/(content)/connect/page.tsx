1f1e9d323d2fb7322c8050d8775e0b5d4795db71
Khinshan Khan <shan@uptogood.dev>
2024-03-06 21:11:25 -0500

    fix linting issue

--- app/(content)/connect/page.tsx
+++ app/(content)/connect/page.tsx
@@ -7,9 +7,9 @@ export default function Connect() {
     <main>
       <Callout variant="note" heading="Work in Progress">
         <p>
-          I'm in the process of rewriting my website. You can view the progress on{" "}
-          <Link href="https://github.com/khinshankhan/www">GitHub</Link>. It'll take some time, so
-          this is a placeholder until then! Soon <sup>TM</sup>
+          {"I'm"} in the process of rewriting my website. You can view the progress on{" "}
+          <Link href="https://github.com/khinshankhan/www">GitHub</Link>. {"It'll"} take some time,
+          so this is a placeholder until then! Soon <sup>TM</sup>
         </p>
       </Callout>
     </main>
--- app/(content)/writings/page.tsx
+++ app/(content)/writings/page.tsx
@@ -6,9 +6,9 @@ export default function Writings() {
     <main>
       <Callout variant="note" heading="Work in Progress">
         <p>
-          I'm in the process of converting my previous articles from org mode and loose messages to
-          mdx. It'll take some time, so articles won't be here for a while... some may trinkle in
-          slowly but it'll all be up eventually... soon <sup>TM</sup>
+          {"I'm"} in the process of converting my previous articles from org mode and loose messages
+          to mdx. {"It'll"} take some time, so articles {"won't"} be here for a while... some may
+          trinkle in slowly but {"it'll"} all be up eventually... soon <sup>TM</sup>
         </p>
       </Callout>
     </main>
