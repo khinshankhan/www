2e672b738f00811e03d1439237beae7552c72b08
Khinshan Khan <shan@uptogood.dev>
2024-03-17 21:24:08 -0400

    account for linting warnings

--- app/(content)/writings/page.tsx
+++ app/(content)/writings/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from "next/image"
 import { ContentData } from "@/schemas/content"
 import { getContentDataBySource } from "@/lib/content"
 import { cn } from "@/lib/utils"
@@ -12,12 +13,14 @@ function WritingCard({ content }: { content: ContentData }) {
       key={content.slug}
       className="card-link link-box flex w-full flex-col gap-2 rounded-lg bg-card p-4 text-card-foreground shadow-lg md:flex-row"
     >
-      <img
-        alt=""
+      <Image
+        alt="games placeholder"
         src="https://hb.imgix.net/41ff19a5a115ce7c78207f166f069b3ab69ed00f.jpg?auto=compress,format&fit=crop&h=600&w=1200&s=bac8a397bb20bed183ded6fdb5b7ebb1"
         className="h-[9.5rem] w-full rounded-lg object-cover md:w-auto"
+        height={600}
+        width={1200}
       />
-      <div className="ml-4 flex flex-grow flex-col justify-between">
+      <div className="ml-4 flex grow flex-col justify-between">
         <h3 className={typographyVariants({ variant: "h3" })}>
           <Link href={`/${content.slug}`} className="link-overlay" nav={true} underline={false}>
             {content.frontmatter.title}
