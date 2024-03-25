0e73118606f03d8b634d591e7c4c588c8eef7ba7
Khinshan Khan <shan@uptogood.dev>
2024-03-24 22:43:16 -0400

    attempt to account for all frontmatter

--- schemas/content.ts
+++ schemas/content.ts
@@ -1,27 +1,62 @@
 import { z } from "zod"
 
+const ImageSchema = z.object({ url: z.string(), alt: z.string() })
+
 export const ContentFrontmatterSchema = z.object({
+  // required
   title: z.string(),
   subtitle: z.string(),
+
+  dateCreated: z.instanceof(Date),
   datePublished: z.instanceof(Date),
   dateModified: z.instanceof(Date),
+
+  // seo
   description: z.string().optional(),
-  // tags: z.array(z.string()).optional().default([]),
-  // draft: z.boolean().optional().default(false),
-})
+  keywords: z.array(z.string()).optional(),
+  coverImage: ImageSchema.optional().default({
+    url: "/images/placeholder.png",
+    alt: "Placeholder image",
+  }),
 
-const TocSchema = z.object({
-  id: z.string(),
-  text: z.string(),
-  level: z.number().min(1).max(6),
-})
+  og: z
+    .object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+      image: ImageSchema.optional(),
+    })
+    .optional(),
 
-export const ComputedDataSchema = z.object({
-  excerpt: z.string(),
-  toc: z.array(TocSchema),
+  ld: z
+    .object({
+      type: z.enum(["WebPage", "BlogPosting", "CollectionPage"]).optional().default("WebPage"),
+      name: z.string().optional(),
+      description: z.string().optional(),
+    })
+    .optional()
+    .default({ type: "WebPage" }),
+
+  // misc
+  draft: z.boolean().optional().default(false),
+  priority: z.number().optional().default(0),
+  tags: z.array(z.string()).optional().default([]),
+
+  // for blogpostings
+  recommended: z.array(z.string()).optional().default([]),
+
+  // for collectionpages
+  collection: z.array(z.string()).optional().default([]),
 })
 
 export type ContentFrontmatter = z.infer<typeof ContentFrontmatterSchema>
+
+export const ComputedDataSchema = z.object({
+  excerpt: z.string(),
+  toc: z.array(z.object({ id: z.string(), text: z.string(), level: z.number().min(1).max(6) })),
+  // TODO: undecided if I want to use these
+  // wordCount: z.number().optional(),
+  // readingTime: z.number().optional(),
+})
 
 export const ContentSourceTypes = ["root", "writings", "projects"] as const
 export type ContentSource = (typeof ContentSourceTypes)[number]
