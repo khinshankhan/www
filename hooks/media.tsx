30bb43ac4e6910d01486eeb1f9f557238cbab412
Khinshan Khan <shan@uptogood.dev>
2024-06-21 05:53:16 -0400

    breakpoint queries

--- hooks/media.tsx
+++ hooks/media.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useLayoutEffect, useState } from "react"
+import { useMediaQuery } from "react-responsive"
 
 export const isServer = typeof window === "undefined" || !window.navigator
 export const isBrowser = !isServer
@@ -11,4 +12,23 @@ export function useMounted() {
   useIsomorphicEffect(() => setMounted(true), [])
 
   return mounted
+}
+
+// NOTE: this needs to be kept in sync with the tailwind config
+const breakpoints = {
+  xss: "(min-width: 320px)",
+  xs: "(min-width: 392px)",
+  sm: "(min-width: 640px)",
+  md: "(min-width: 768px)",
+  lg: "(min-width: 1024px)",
+  xl: "(min-width: 1325px)",
+  "2xl": "(min-width: 1536px)",
+  isMobile: "(max-width: 767px)",
+  isDesktop: "(min-width: 768px)",
+}
+
+// NOTE: this is a 'hacky way' to get the breakpoints for controlled components, rely on css as much a spossible
+export function useBreakpoint({ breakpoint }: { breakpoint: keyof typeof breakpoints }) {
+  const query = breakpoints[breakpoint]
+  return useMediaQuery({ query })
 }
