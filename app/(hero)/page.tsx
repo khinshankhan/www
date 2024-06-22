90564d2cd53242bd291c693aeb99b0641977ee25
Khinshan Khan <shan@uptogood.dev>
2024-06-22 16:40:53 -0400

    homepage

--- app/(hero)/page.tsx
+++ app/(hero)/page.tsx
@@ -1,5 +1,113 @@
-import React from "react"
+import Image from "next/image"
+import { cn } from "@/lib/utils"
+import { Link } from "@/components/primitives/link"
+import { typographyVariants } from "@/components/primitives/typography"
 
 export default function Home() {
-  return <div>home</div>
+  return (
+    <section className="page-container mx-auto mt-8 flex flex-col items-center justify-center space-y-20 md:mt-20">
+      <header className="flex flex-col items-center justify-center gap-4 md:flex-row-reverse">
+        <ProfileImage />
+
+        <IntroSection />
+      </header>
+
+      <MainContent />
+    </section>
+  )
+}
+
+function ProfileImage() {
+  return (
+    <Image
+      alt="Profile Image"
+      className="rounded-full md:ml-8"
+      width={256}
+      height={256}
+      src="https://github.com/ghost.png"
+      style={{
+        aspectRatio: "256/256",
+        objectFit: "cover",
+      }}
+    />
+  )
+}
+
+function Wave() {
+  return (
+    <img
+      className="inline aspect-auto size-[1em] animate-wave"
+      src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png?v8"
+      alt=":wave:"
+      aria-label=":wave:"
+      data-type="emoji"
+      draggable="false"
+      height="72px"
+      width="72px"
+    />
+  )
+}
+
+function IntroSection() {
+  return (
+    <section className="flex flex-col items-center space-y-8 text-center md:items-end md:text-right">
+      <div>
+        <h1
+          className={cn(
+            typographyVariants({ variant: "h1", className: "font-extrabold md:whitespace-nowrap" })
+          )}
+        >
+          Hello there <Wave />
+        </h1>
+        <h2 className={cn(typographyVariants({ variant: "h2", className: "font-extrabold" }))}>
+          {"I'm"} Khinshan Khan
+        </h2>
+      </div>
+      <h3
+        className={cn(
+          typographyVariants({ variant: "h3" }),
+          "mx-auto max-w-[700px] hyphens-auto text-balance font-normal sm:hyphens-none"
+        )}
+      >
+        Exploring the intersections of creativity and technology
+      </h3>
+
+      <Link
+        variant="toc"
+        href="/writings/"
+        className="w-full rounded-lg bg-secondary py-3 text-secondary-foreground shadow-lg transition-colors duration-300 ease-in-out hover:bg-secondary/90 md:w-auto md:px-8 md:text-lg"
+      >
+        Read my writings
+      </Link>
+    </section>
+  )
+}
+
+function MainContent() {
+  return (
+    <section
+      className={cn(
+        typographyVariants({ variant: "h4" }),
+        "mx-auto max-w-[1400px] space-y-8 hyphens-auto text-center font-normal sm:hyphens-none [&>*]:mx-8 [&>*]:rounded-lg [&>*]:bg-background/10 [&>*]:p-6 [&>*]:backdrop-blur-[3px]"
+      )}
+    >
+      <p>
+        Welcome to my little corner of the internet! Here, I share my thoughts and musings. Whether
+        you learn something new or just have a bit of fun, I hope you enjoy your stay.
+      </p>
+
+      <p>
+        Landing pages can be a bit bland with all that whitespace, so here’s a little engagement
+        hook: this website has 0 hidden easter eggs. Can you find them all? Happy hunting!
+      </p>
+
+      <p>
+        Meanwhile, feel free to check out my <Link href="/writings/">writings</Link> or{" "}
+        <Link href="/projects/">projects</Link>. If you’d like to get in touch, you can{" "}
+        <Link href="/connect/">connect with me</Link>.
+      </p>
+
+      <p>Enjoy exploring!</p>
+    </section>
+  )
 }
