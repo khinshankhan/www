51b6df4557bd971593e2fff45cc21eb912b89932
Khinshan Khan <shan@uptogood.dev>
2024-06-23 00:34:05 -0400

    remove unused components

--- components/primitives/typography.tsx
+++ components/primitives/typography.tsx
@@ -1,8 +1,4 @@
-import React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
-import { Slot } from "@radix-ui/react-slot"
-import { Link } from "./link"
 
 export const typographyVariants = cva("", {
   variants: {
@@ -25,45 +21,3 @@ export const typographyVariants = cva("", {
 })
 
 export type TypographyVariants = VariantProps<typeof typographyVariants>
-
-export interface TextProps extends React.HTMLAttributes<HTMLSpanElement>, TypographyVariants {
-  asChild?: boolean
-}
-
-export const Text = React.forwardRef<HTMLSpanElement, TextProps>(
-  ({ className = "", variant, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "span"
-    return <Comp className={cn(typographyVariants({ variant, className }))} ref={ref} {...props} />
-  }
-)
-Text.displayName = "Text"
-
-export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>, TypographyVariants {
-  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
-  asChild?: boolean
-}
-
-export const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ className = "", variant, as: asProp = "h1", asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : asProp
-    return <Comp className={cn(typographyVariants({ variant, className }))} ref={ref} {...props} />
-  }
-)
-Heading.displayName = "Heading"
-
-interface AnchorHeadingProps extends HeadingProps {
-  id: string
-}
-// TODO: implement this
-export const AnchorHeading = React.forwardRef<HTMLHeadingElement, AnchorHeadingProps>(
-  ({ as: asProp, id, className = "", children, ...props }, ref) => {
-    return (
-      <Heading as={asProp} id={id} variant={asProp} className={cn(className)} ref={ref} {...props}>
-        <Link href={`#${id}`} className="anchor-link" isMonochrome>
-          {children}
-        </Link>
-      </Heading>
-    )
-  }
-)
-AnchorHeading.displayName = "AnchorHeading"
