93ecdd28051c2a5053f29c7888cf45e8b7ab2b86
Khinshan Khan <shan@uptogood.dev>
2024-09-26 22:59:13 -0400

    In the beginning there was darkness

--- app/(content)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function ContentLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex grow flex-col bg-content text-content-foreground">{children}</main>
-}
