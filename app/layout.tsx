bdfd1346181f2f740c9b2fd4b4688082344f56b3
Khinshan Khan <shan@uptogood.dev>
2024-10-05 15:21:59 -0400

    setup simple next project

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body>
+        <p>Hello there!</p>
+        {children}
+      </body>
+    </html>
+  );
+}
