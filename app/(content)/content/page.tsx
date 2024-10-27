c7fbd8c5f8a25c7edd27331fc6e5a3cecb633324
Khinshan Khan <shan@uptogood.dev>
2024-10-27 06:49:14 -0400

    first draft of containers

--- app/(content)/content/page.tsx
+++ app/(content)/content/page.tsx
@@ -14,49 +14,57 @@ export default async function Page() {
         </Text>
       </header>
 
-      <main className="py-6">
-        <section className="content-container my-4">
-          <p>
-            The goal of typography is to relate font size, line height, and line width in a
-            proportional way that maximizes beauty and makes reading easier and more pleasant.
-          </p>
+      <main className="content-container py-6">
+        <p>
+          Hello there. This is where experiments sprout and blossom. Some even become features! Or
+          they may become bugs that then become features. Or they... well yea, stuff happens.
+        </p>
 
-          <Text id="foo">hi</Text>
+        <p>
+          The goal of typography is to relate font size, line height, and line width in a
+          proportional way that maximizes beauty and makes reading easier and more pleasant.
+        </p>
 
-          <div className="w-full space-y-4 bg-accent-4 py-4">
-            <p>I wanna be full width</p>
+        <Text id="foo">hi</Text>
 
-            <p>I wanna be full width 2</p>
-          </div>
-          <p>
-            The <Strong>Golden Ratio</Strong> is a mathematical ratio that is commonly found in
-            nature. It can be used to create pleasing, natural-looking compositions in your design
-            work.
-          </p>
-          <p>
+        <div className="fullo w-full space-y-4 bg-accent-4 py-4">
+          <p>I wanna be full width</p>
+
+          <p>I wanna be full width 2</p>
+        </div>
+
+        <div className="mobile-full-width-container bg-accent-4 py-4">
+          <p>I should be small again</p>
+          <p>I should be small again</p>
+        </div>
+
+        <p>
+          The <Strong>Golden Ratio</Strong> is a mathematical ratio that is commonly found in
+          nature. It can be used to create pleasing, natural-looking compositions in your design
+          work.
+        </p>
+        <p>
+          Cherry bananas rainbow sprinkles, hot fudge cookie dough blueberries strawberries french
+          vanilla oreos. Strawberry strawberries blueberries pistachio chocolate Pistachio mint
+          chocolate chip rocky road peanut butter strawberries coffee vanilla chocolate chip cookie
+          dough almonds. Neopolitan chocolate chip caramel syrup, french vanilla gummy worms bananas
+          oreos oreos. Strawberry salted caramel syrup chocolate cake batter whipped cream cherry
+          rocky road gummy worms Strawberry chocolate sprinkles blueberries dark chocolate cake
+          batter. Cookies and cream mint chocolate chip cookies and cream vanilla kitkat peanuts,
+          mint cake batter. Caramel syrup peanuts mint chocolate chip peanuts.
+        </p>
+        {[].map((i) => (
+          <p key={i}>
             Cherry bananas rainbow sprinkles, hot fudge cookie dough blueberries strawberries french
+            Chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
+            chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
+            chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
+            chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
+            chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
+            chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie dough
             vanilla oreos. Strawberry strawberries blueberries pistachio chocolate Pistachio mint
-            chocolate chip rocky road peanut butter strawberries coffee vanilla chocolate chip
-            cookie dough almonds. Neopolitan chocolate chip caramel syrup, french vanilla gummy
-            worms bananas oreos oreos. Strawberry salted caramel syrup chocolate cake batter whipped
-            cream cherry rocky road gummy worms Strawberry chocolate sprinkles blueberries dark
-            chocolate cake batter. Cookies and cream mint chocolate chip cookies and cream vanilla
-            kitkat peanuts, mint cake batter. Caramel syrup peanuts mint chocolate chip peanuts.
           </p>
-          {[].map((i) => (
-            <p key={i}>
-              Cherry bananas rainbow sprinkles, hot fudge cookie dough blueberries strawberries
-              french Chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough chocolate chip cookie dough chocolate chip cookie dough chocolate chip cookie
-              dough vanilla oreos. Strawberry strawberries blueberries pistachio chocolate Pistachio
-              mint
-            </p>
-          ))}
-        </section>
+        ))}
       </main>
     </article>
   )
