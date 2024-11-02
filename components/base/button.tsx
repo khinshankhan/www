e7a4ce2ce22bbaa0a9967d1dde973a1a179c2e5b
Khinshan Khan <shan@uptogood.dev>
2024-11-02 15:27:45 -0400

    button component

---
+++ components/base/button.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
+import { Slot } from "@radix-ui/react-slot"
+
+export const buttonVariants = cva(
+  "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-[var(--celestial-4)] hover:bg-[var(--celestial-6)]/40",
+        ghost: "hover:bg-[var(--celestial-6)]/40",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "size-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+// prettier-ignore
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+export function Button({ className, variant, size, asChild = false, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : "button"
+
+  return (
+    <Comp className={cn(buttonVariants({ variant, size, className }))} type="button" {...props} />
+  )
+}
