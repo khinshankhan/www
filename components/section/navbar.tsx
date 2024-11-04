d0397201ff6220f0b486461490cb8652cb2774a7
Khinshan Khan <shan@uptogood.dev>
2024-11-03 19:31:55 -0500

    mode toggle

--- components/section/navbar.tsx
+++ components/section/navbar.tsx
@@ -3,9 +3,19 @@
 import React from "react"
 import NextLink from "next/link"
 import { usePathname } from "next/navigation"
+import { Button } from "@/components/base/button"
+import { MonitorCog, Moon, Sun, type IconProps } from "@/components/base/icon"
 import { Logo } from "@/components/base/logo"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/composite/dropdown-menu"
 import { SmartLink } from "@/components/composite/smart-link"
-import { cn } from "@/lib/utils"
+import { capitalize, cn } from "@/lib/utils"
+import { useTheme } from "next-themes"
 
 const links = [
   { label: "About", href: "/about/" },
@@ -30,6 +40,50 @@ function NavLinks({ className = "" }: { className?: string }) {
   )
 }
 
+function ModeIcon({ theme, ...props }: { theme: string } & IconProps) {
+  if (theme === "light") return <Sun {...props} />
+  if (theme === "dark") return <Moon {...props} />
+  return <MonitorCog {...props} />
+}
+
+function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" aria-label="Toggle theme">
+          <ModeIcon
+            theme={theme ?? "system"}
+            className="size-[1.2rem]"
+            role="presentation"
+            aria-hidden="true"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent side="bottom" align="end">
+        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+          {["light", "dark", "system"].map((value) => (
+            <DropdownMenuRadioItem key={value} value={value}>
+              <span className="flex w-full flex-row items-center justify-between">
+                <span>{capitalize(value)}</span>
+
+                <ModeIcon
+                  theme={value}
+                  className="size-[1.2rem]"
+                  role="presentation"
+                  aria-hidden="true"
+                />
+              </span>
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 export function Navbar() {
   return (
     <header className="bounded-page-layout flex min-h-[68px] items-center pt-2 md:min-h-[78px] lg:min-h-[88px]">
@@ -42,6 +96,15 @@ export function Navbar() {
         {/* rhs on desktop view */}
         <div className="hide-mobile flex flex-row items-center gap-4">
           <NavLinks />
+
+          <div className="flex flex-row gap-4">
+            <ModeToggle />
+          </div>
+        </div>
+
+        {/* rhs on mobile view */}
+        <div className="show-mobile flex flex-col-reverse gap-2 xss:flex-row">
+          <ModeToggle />
         </div>
       </nav>
     </header>
--- lib/utils.tsx
+++ lib/utils.tsx
@@ -8,6 +8,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
 /* url utils */
 
 export function isInternalLink(href: LinkProps["href"]) {
