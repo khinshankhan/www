eca43e7f40de7e958f5791ce5d6e0827db77d272
Khinshan Khan <shan@uptogood.dev>
2024-11-04 20:27:11 -0500

    poc sidebar layout

--- components/template/content-layout.tsx
+++ components/template/content-layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { PageHeader, type PageHeaderProps } from "@/components/section/page-header"
+import { WithSidebar } from "@/components/section/with-sidebar"
 
 export function ContentLayout({
   title,
@@ -10,8 +11,13 @@ export function ContentLayout({
     <article className="flex grow flex-col">
       <PageHeader title={title} subtitle={subtitle} />
 
-      <div className="bounded-content-layout my-1 grow py-12">
-        <div className="prose min-w-full">{children}</div>
+      <div className="bounded-content-layout my-1 flex grow flex-col py-12">
+        <WithSidebar
+          direction="right"
+          sidebar={<div>Sample sidebar plus some lorem ipsum to showcase wrapping</div>}
+        >
+          <div className="prose min-w-full">{children}</div>
+        </WithSidebar>
       </div>
     </article>
   )
