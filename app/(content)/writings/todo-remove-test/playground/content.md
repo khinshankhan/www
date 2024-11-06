c8b4ac4e2099d28999fec4b69a04d7097dabf2ac
Khinshan Khan <shan@uptogood.dev>
2024-11-06 08:47:26 -0500

    style heading components

--- app/(content)/writings/todo-remove-test/playground/content.md
+++ app/(content)/writings/todo-remove-test/playground/content.md
@@ -3,4 +3,11 @@ title: Playground v2
 subtitle: Where Things are Perfectly Broken
 ---
 
-I like icecream... with a cherry on top! From mdx :smirk:
+Hello there. This is where experiments sprout and blossom. Some even become features! Or they may become bugs that then
+become features. Or they... well yea, stuff happens.
+
+## New Heading
+
+Yay!
+
+<h3>Not a markdown heading hmm</h3>
--- app/(content)/writings/todo-remove-test/playground/page.tsx
+++ app/(content)/writings/todo-remove-test/playground/page.tsx
@@ -1,8 +1,11 @@
 import fs from "fs"
 import path from "path"
 import React from "react"
+import { typographyVariants } from "@/components/base/typography"
 import { ContentLayout } from "@/components/template/content-layout"
+import { cn } from "@/lib/utils"
 import matter from "gray-matter"
+import type { MDXComponents } from "mdx/types"
 import { MDXRemote } from "next-mdx-remote/rsc"
 
 const projectRoot = process.cwd()
@@ -15,6 +18,40 @@ const thisDir = path.join(
   "playground"
 )
 
+const components: MDXComponents = {
+  // TODO: look into why heading component isn't compatible with MDX headings
+  h2: ({ className = "", ...props }) => (
+    <h2
+      {...props}
+      className={cn("scroll-mt-5", typographyVariants({ variant: "h2" }), className)}
+    />
+  ),
+  h3: ({ className = "", ...props }) => (
+    <h3
+      {...props}
+      className={cn("scroll-mt-5", typographyVariants({ variant: "h3" }), className)}
+    />
+  ),
+  h4: ({ className = "", ...props }) => (
+    <h4
+      {...props}
+      className={cn("scroll-mt-5", typographyVariants({ variant: "h4" }), className)}
+    />
+  ),
+  h5: ({ className = "", ...props }) => (
+    <h5
+      {...props}
+      className={cn("scroll-mt-5", typographyVariants({ variant: "h5" }), className)}
+    />
+  ),
+  h6: ({ className = "", ...props }) => (
+    <h6
+      {...props}
+      className={cn("scroll-mt-5", typographyVariants({ variant: "h6" }), className)}
+    />
+  ),
+}
+
 export default async function Page() {
   const absFilePath = path.join(thisDir, "./content.md")
   const fileContent = await fs.promises.readFile(absFilePath, "utf8")
@@ -26,7 +63,7 @@ export default async function Page() {
       subtitle={data.subtitle}
       ghPath="/app/(content)/writings/todo-remove-test/playground/content.tsx"
     >
-      <MDXRemote source={content} />
+      <MDXRemote source={content} components={components} />
     </ContentLayout>
   )
 }
