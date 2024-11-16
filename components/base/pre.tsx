00a6516bd1ff8eb8ed7c07974639fd0a3f670923
Khinshan Khan <shan@uptogood.dev>
2024-11-15 19:06:18 -0500

    scrollable pre block

--- components/base/pre.tsx
+++ components/base/pre.tsx
@@ -1,20 +1,22 @@
 import React from "react"
+import { ScrollArea, ScrollBar, ScrollViewport } from "@/components/base/scroll-area"
 import { cn } from "@/lib/utils"
 
 // prettier-ignore
-export interface PreProps extends React.ComponentPropsWithoutRef<"pre"> {
+export interface PreProps extends React.ComponentPropsWithRef<"pre"> {
 }
 
 export function Pre({ className = "", ...props }: PreProps) {
-  console.log({ props })
-
   return (
-    <pre
-      className={cn(
-        "rounded-md border border-muted bg-muted/30 p-2 text-sm md:text-base lg:text-xl [&>code]:contents",
-        className
-      )}
-      {...props}
-    />
+    <ScrollArea className="w-full" type="auto">
+      <ScrollViewport className="size-full rounded-md border border-muted bg-muted/30 p-2">
+        <pre
+          className={cn("text-sm md:text-base lg:text-xl [&>code]:contents", className)}
+          {...props}
+        />
+      </ScrollViewport>
+
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   )
 }
---
+++ components/base/scroll-area.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import React from "react"
+import { cn } from "@/lib/utils"
+import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
+
+// prettier-ignore
+type ScrollViewportProps = React.ComponentPropsWithRef<typeof ScrollAreaPrimitive.Viewport>
+
+export function ScrollViewport({ className = "", children, ...props }: ScrollViewportProps) {
+  return (
+    <ScrollAreaPrimitive.Viewport
+      className={cn("size-full rounded-[inherit]", className)}
+      {...props}
+    >
+      {children}
+    </ScrollAreaPrimitive.Viewport>
+  )
+}
+
+// prettier-ignore
+type ScrollAreaProps = React.ComponentPropsWithRef<typeof ScrollAreaPrimitive.Root>
+
+export function ScrollArea({ className = "", children, ...props }: ScrollAreaProps) {
+  return (
+    <ScrollAreaPrimitive.Root className={cn("overflow-hidden", className)} {...props}>
+      {children}
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
+  )
+}
+
+// prettier-ignore
+type ScrollBarProps = React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Scrollbar>
+
+export function ScrollBar({ className, orientation = "vertical", ...props }: ScrollBarProps) {
+  return (
+    <ScrollAreaPrimitive.Scrollbar
+      orientation={orientation}
+      className={cn(
+        "flex touch-none p-0.5 transition-colors duration-[160ms] ease-out select-none hover:bg-background-1/30",
+        orientation === "vertical" && "w-2.5 flex-row",
+        orientation === "horizontal" && "h-2.5 flex-col",
+        // orientation === "vertical" && "h-full w-1.5",
+        // orientation === "horizontal" && "h-1.5 flex-col",
+        className
+      )}
+      {...props}
+    >
+      <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-accent-link/70" />
+    </ScrollAreaPrimitive.Scrollbar>
+  )
+}
