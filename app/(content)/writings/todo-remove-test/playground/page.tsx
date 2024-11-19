385efd099fb955d454c04f852b29e19109bd3deb
Khinshan Khan <shan@uptogood.dev>
2024-11-19 17:31:48 -0500

    move mdx rendering logic to template

--- app/(content)/writings/todo-remove-test/playground/page.tsx
+++ app/(content)/writings/todo-remove-test/playground/page.tsx
@@ -1,92 +1,19 @@
 import fs from "fs"
 import path from "path"
 import React from "react"
-import { Code } from "@/components/base/code"
-import { Image } from "@/components/base/image"
-import { Spoiler } from "@/components/base/spoiler"
-import { typographyVariants } from "@/components/base/typography"
-import { SmartLink } from "@/components/composite/smart-link"
-import { Pre } from "@/components/section/pre"
-import { Tabbify } from "@/components/section/tabbify"
 import { Toc } from "@/components/section/toc"
 import { ContentLayout } from "@/components/template/content-layout"
-import { rehypeSectionizeByHeading } from "@/lib/mdx-plugins/rehype-sectionize-by-heading"
-import { rehypeSlug } from "@/lib/mdx-plugins/rehype-slug"
-import { remarkExcerptExport, remarkMarkFirstParagraph } from "@/lib/mdx-plugins/remark-excerpt"
+import { MDXRenderer } from "@/components/template/mdx-renderer"
+import { remarkExcerptExport } from "@/lib/mdx-plugins/remark-excerpt"
 import { remarkPrependTopHeading } from "@/lib/mdx-plugins/remark-prepend-top-heading"
 import { remarkTocExport, type TocItem } from "@/lib/mdx-plugins/remark-toc-export"
 import { ContentFrontmatterSchema, getContentSource } from "@/lib/schemas/content"
 import { cn } from "@/lib/utils"
 import matter from "gray-matter"
-import type { MDXComponents } from "mdx/types"
-import { MDXRemote } from "next-mdx-remote/rsc"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import { remark } from "remark"
-import remarkGfm from "remark-gfm"
 
 const projectRoot = process.cwd()
 const projectDir = path.join(projectRoot)
-
-const components: MDXComponents = {
-  a: ({ href = "#", children = null, ...props }) => (
-    <SmartLink href={href} {...props}>
-      {children}
-    </SmartLink>
-  ),
-  // TODO: look into why heading component isn't compatible with MDX headings
-  h2: ({ className = "", children, ...props }) => (
-    <h2 {...props} className={cn("scroll-mt-5", typographyVariants({ variant: "h2" }), className)}>
-      {children}
-    </h2>
-  ),
-  h3: ({ className = "", children, ...props }) => (
-    <h3 {...props} className={cn("scroll-mt-5", typographyVariants({ variant: "h3" }), className)}>
-      {children}
-    </h3>
-  ),
-  h4: ({ className = "", children, ...props }) => (
-    <h4 {...props} className={cn("scroll-mt-5", typographyVariants({ variant: "h4" }), className)}>
-      {children}
-    </h4>
-  ),
-  h5: ({ className = "", children, ...props }) => (
-    <h5 {...props} className={cn("scroll-mt-5", typographyVariants({ variant: "h5" }), className)}>
-      {children}
-    </h5>
-  ),
-  h6: ({ className = "", children, ...props }) => (
-    <h6 {...props} className={cn("scroll-mt-5", typographyVariants({ variant: "h6" }), className)}>
-      {children}
-    </h6>
-  ),
-  code: ({ ref: _ref, children, ...props }) => {
-    // surely there's a better way to do this?
-    const language =
-      props.className
-        ?.split(" ")
-        .find((c) => c.startsWith("language-"))
-        ?.split("language-")
-        .pop() || "plaintext"
-
-    return (
-      <Code language={language} {...props}>
-        {children as string}
-      </Code>
-    )
-  },
-  pre: ({ ref: _ref, ...props }) => {
-    // @ts-ignore: it's fine, this is how mdx codeblocks work supposedly
-    const text = props.children?.props?.children as string
-
-    return <Pre {...props} text={text} />
-  },
-
-  Spoiler,
-  Tabbify,
-  Image,
-
-  Test: ({ className = "" }) => <div className={cn(className)}>this was a test and you passed</div>,
-}
 
 export default async function Page() {
   const filePath = path.join("content", "writings", "todo-remove-test", "playground", "page.md")
@@ -141,54 +68,7 @@ export default async function Page() {
       childrenWrappingClass={cn("prose", showToc && "mt-6 xl:mt-2")}
       sidebar={showToc && <Toc headings={metadata.computed.toc} />}
     >
-      <MDXRemote
-        source={content}
-        components={components}
-        options={{
-          mdxOptions: {
-            remarkPlugins: [
-              remarkMarkFirstParagraph,
-              [
-                remarkPrependTopHeading,
-                {
-                  depth: 2,
-                  text: "Introduction",
-                  properties: {
-                    className: "sr-only m-0",
-                  },
-                },
-              ],
-              [remarkGfm, { singleTilde: false }],
-            ],
-            rehypePlugins: [
-              [
-                rehypeSlug,
-                {
-                  reservedIds: ["excerpt"],
-                },
-              ],
-              [
-                rehypeSectionizeByHeading,
-                {
-                  isFlat: false,
-                  sectionProperties: {
-                    className: "prose",
-                  },
-                },
-              ],
-              [
-                rehypeAutolinkHeadings,
-                {
-                  behavior: "wrap",
-                  properties: {
-                    className: ["anchor-link"],
-                  },
-                },
-              ],
-            ],
-          },
-        }}
-      />
+      <MDXRenderer source={content} />
     </ContentLayout>
   )
 }
