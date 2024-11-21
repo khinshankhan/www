286c9146cbc290d8722fd9e5b6d1610b18d90442
Khinshan Khan <shan@uptogood.dev>
2024-11-20 21:22:52 -0500

    dont need a utility if it's one time use

--- lib/mdx-plugins/remark-excerpt.tsx
+++ lib/mdx-plugins/remark-excerpt.tsx
@@ -2,7 +2,6 @@ import type { Root as MdastRoot } from "mdast"
 import { toString } from "mdast-util-to-string"
 import type { Transformer } from "unified"
 import { EXIT, visit } from "unist-util-visit"
-import { setNodeProperty } from "./utils"
 
 type Options = {
   id: string
@@ -24,7 +23,16 @@ export function remarkMarkFirstParagraph(options?: Options): Transformer<MdastRo
         return
       }
 
-      node = setNodeProperty(node, "id", settings.id)
+      node.data = node.data || {}
+      node.data.hProperties = node.data.hProperties || {}
+
+      // NOTE: types are dumb, Data represents information associated by the ecosystem with the node.
+      // This space is guaranteed to never be specified by unist or specifications implementing unist.
+      // Would be nice if the types were like Record<string, unknown> instead but it's not.
+      // https://github.com/syntax-tree/unist/blob/main/readme.md#data
+      // @ts-expect-error: variable attributes is not in the Data type
+      node.data.id = settings.id
+      node.data.hProperties.id = settings.id
       return EXIT
     })
   }
