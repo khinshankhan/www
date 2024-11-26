5ef8e93093186d84de0bb8f0a941f415c003b7c3
Khinshan Khan <shan@uptogood.dev>
2024-11-26 18:18:40 -0500

    tooltip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { TooltipProvider } from "@/components/base/tooltip"
 import { typographyVariants } from "@/components/base/typography"
 import { Navbar } from "@/components/section/navbar"
 import { cn } from "@/lib/utils"
@@ -35,17 +36,19 @@ export default async function Layout({ children }: { children: React.ReactNode }
           enableSystem
           disableTransitionOnChange
         >
-          <div vaul-drawer-wrapper="">
-            <div className="relative isolate z-0 flex min-h-[87vh] flex-col xs:min-h-[96vh]">
-              <Navbar />
-              {/* NOTE: assumes pages will be wrapped in main tags with background color + grow */}
-              <div className="relative flex grow flex-col">{children}</div>
+          <TooltipProvider>
+            <div vaul-drawer-wrapper="">
+              <div className="relative isolate z-0 flex min-h-[87vh] flex-col xs:min-h-[96vh]">
+                <Navbar />
+                {/* NOTE: assumes pages will be wrapped in main tags with background color + grow */}
+                <div className="relative flex grow flex-col">{children}</div>
+              </div>
+              <footer className="bounded-page-layout h-40">
+                <p>Footer content here</p>
+              </footer>
             </div>
-            <footer className="bounded-page-layout h-40">
-              <p>Footer content here</p>
-            </footer>
-          </div>
-          <BreakpointsIndicator />
+            <BreakpointsIndicator />
+          </TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
--- components/composite/emoji.tsx
+++ components/composite/emoji.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from "@/components/base/tooltip"
+import { typographyVariants } from "@/components/base/typography"
 import { emojiLookup, type EmojiKey } from "@/lib/emoji"
 import { cn } from "@/lib/utils"
 import { InvalidEmojiException } from "@khinshankhan/emoji-helper-core"
@@ -11,24 +13,51 @@ interface EmojiProps {
   wrapperClassName?: string
 }
 export function Emoji({ name, className = "", wrapperClassName = "" }: EmojiProps) {
+  const [hovering, setHovering] = useState(false)
+  const [clicked, setClicked] = useState(false)
+
   const emojiInfo = emojiLookup.get(name)
   if (emojiInfo === undefined) {
     throw new InvalidEmojiException(`Emoji not found ${name}`)
   }
 
+  const handleMouseEnter = () => setHovering(true)
+  const handleMouseLeave = () => setHovering(false)
+
+  const isTooltipOpen = hovering || clicked
+
   return (
     <span className={wrapperClassName}>
-      {/* eslint-disable @next/next/no-img-element */}
-      <img
-        className={cn("inline aspect-auto size-[1em]", className)}
-        src={emojiInfo.url}
-        alt={emojiInfo.char || emojiInfo.alt}
-        aria-label={emojiInfo.alt}
-        data-type="emoji"
-        draggable="false"
-        height="72px"
-        width="72px"
-      />
+      <Tooltip open={isTooltipOpen}>
+        <TooltipTrigger
+          asChild
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => setClicked((prev) => !prev)}
+        >
+          {/* eslint-disable @next/next/no-img-element */}
+          <img
+            className={cn("inline aspect-auto size-[1em]", className)}
+            src={emojiInfo.url}
+            alt={emojiInfo.char || emojiInfo.alt}
+            aria-label={emojiInfo.alt}
+            data-type="emoji"
+            draggable="false"
+            height="72px"
+            width="72px"
+          />
+        </TooltipTrigger>
+        <TooltipContent
+          side="top"
+          className={cn(typographyVariants({ variant: "xs" }), "w-full p-2")}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onPointerDownOutside={() => setClicked(false)}
+        >
+          <span>{emojiInfo.alt}</span>
+          <TooltipArrow className="fill-knockout" width={11} height={5} />
+        </TooltipContent>
+      </Tooltip>
     </span>
   )
 }
