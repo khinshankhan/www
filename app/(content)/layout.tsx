98ab072c8ae4f6123a661f5d8b5e2bda38ea8fff
Khinshan Khan <shan@uptogood.dev>
2025-09-06 21:26:23 -0400

    v52

--- app/(content)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main id="main-content" className="relative flex grow flex-col">
-      {children}
-    </main>
-  )
-}
