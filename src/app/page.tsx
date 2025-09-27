001d8eabdb8502e42e6fef6810857e8fbd36e8d3
Khinshan Khan <shan@uptogood.dev>
2025-09-26 20:51:38 -0400

    still debugging layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,27 @@ export default function Home() {
         </header>
 
         <div className="content-layout grow">
-          <h1>Hello there</h1>
+          {/* make this header "scroll effect" */}
+          <header className="py-14">
+            <h1>Title of the page</h1>
+            <span>Description of page</span>
+          </header>
+
+          <p>Hello there. Content goes here</p>
+
+          {[0, 1, 2, 3, 4, 5].map((_, i) => (
+            <p key={i}>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+              egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+              Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
+              placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+              erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum,
+              elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
+              dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
+              cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam
+              erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+            </p>
+          ))}
         </div>
       </div>
 
