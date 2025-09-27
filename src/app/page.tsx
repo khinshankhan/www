48d737e293eadda25bfa1259f51899f35ec06e11
Khinshan Khan <shan@uptogood.dev>
2025-09-26 22:26:53 -0400

    experimenting with general placement of elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,17 @@ export default function Home() {
   return (
     <div className="flex min-h-dvh w-full flex-col items-center justify-center">
       <div className="xs:min-h-[96vh] flex min-h-[87vh] w-full grow flex-col items-center justify-center">
-        <header className="bg-background/60 page-layout sticky-header relative sticky top-0 z-50 flex justify-center py-4 pt-8 backdrop-blur-md">
-          <nav> Hello there</nav>
+        <header className="bg-background/60 page-layout sticky-header relative sticky top-0 z-50 flex flex-col justify-center py-4 pt-8 backdrop-blur-md">
+          <nav className="align-center flex flex-row justify-between">
+            <div>icon</div>
+
+            <div className="align-center flex flex-row gap-2">
+              {[...Array(4).keys()].map((num) => (
+                <div key={num}>{`Link ${num}`}</div>
+              ))}
+            </div>
+          </nav>
+
           <HorizontalLine className="z-2 reveal-on-scroll absolute bottom-0" />
         </header>
 
@@ -42,23 +51,30 @@ export default function Home() {
           <header className="py-14">
             <h1>Title of the page</h1>
             <span>Description of page</span>
+
+            <HorizontalLine className="z-2 mt-10">
+              93ecdd28051c2a5053f29c7888cf45e8b7ab2b86
+            </HorizontalLine>
           </header>
 
-          <p>Hello there. Content goes here</p>
+          <div className="flex w-full grow flex-col gap-2">
+            <p>Hello there. Content goes here</p>
 
-          {[...Array(13).keys()].map((num) => (
-            <p key={num}>
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-              egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
-              Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
-              placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
-              erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum,
-              elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
-              dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
-              cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam
-              erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
-            </p>
-          ))}
+            {[...Array(13).keys()].map((num) => (
+              <p key={num}>
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
+                ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+                Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+                Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean
+                fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus
+                lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent
+                dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna
+                eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+                facilisis luctus, metus
+              </p>
+            ))}
+          </div>
         </div>
       </div>
 
