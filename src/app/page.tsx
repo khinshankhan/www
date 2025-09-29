17d695120e59aa00a0af41a8a3951b8e815e8cc9
Khinshan Khan <shan@uptogood.dev>
2025-09-28 21:02:29 -0400

    refactor to use semantic elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,43 +59,61 @@ export default function Home() {
           <div className="from-background-1/100 via-background-1/50 to-background-1/0 absolute -bottom-16 h-16 w-full bg-gradient-to-b" />
         </div>
 
-        <div className="isolate flex w-full flex-col items-center">
-          <header className="maxw-content fixed z-0 w-full py-14">
-            <h1>Title of the page</h1>
-            <span>Description of page</span>
-          </header>
-        </div>
+        <main>
+          <article className="isolate flex w-full flex-col items-center">
+            <header className="maxw-content fixed z-0 w-full py-14">
+              <h1>Title of the page</h1>
+              <span>Description of page</span>
+            </header>
 
-        {/* TODO: determine margin based on article header height */}
-        {/* acts as buffer to gradually hide fixed header, also gives header breathing room from content */}
-        <div className="z-1 from-background-1/100 via-background-1/50 to-background-1/0 relative mt-48 h-12 bg-gradient-to-t" />
+            {/* TODO: determine margin based on article header height */}
+            {/* acts as buffer to gradually hide fixed header, also gives header breathing room from content */}
+            <div className="z-1 from-background-1/100 via-background-1/50 to-background-1/0 relative mt-48 h-12 bg-gradient-to-t" />
 
-        <div className="bg-background-2 isolate flex w-full grow flex-col items-center justify-center">
-          <div className="maxw-content relative flex w-full grow flex-col gap-2 py-14">
-            <p>Hello there. Content goes here [gets sourced from mdx files later].</p>
+            <div className="bg-background-2 isolate flex w-full grow flex-col items-center justify-center">
+              <div className="maxw-content relative flex w-full grow flex-col gap-2 py-14">
+                <section aria-labelledby="introduction" className="flex flex-col gap-2">
+                  <h2 id="introduction" className="sr-only pt-8 text-2xl font-bold">
+                    Introduction
+                  </h2>
 
-            {[...Array(13).keys()].map((num) => (
-              <p key={num}>
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
-                ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
-                Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
-                Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean
-                fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus
-                lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent
-                dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna
-                eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-                facilisis luctus, metus
-              </p>
-            ))}
+                  <p>Hello there. Content goes here [gets sourced from mdx files later].</p>
+                </section>
+
+                {[...Array(13).keys()].map((num) => (
+                  <section
+                    key={num}
+                    aria-labelledby={`section-${num}`}
+                    className="flex flex-col gap-2"
+                  >
+                    <h2 id={`section-${num}`} className="pt-8 text-2xl font-bold">
+                      Section {num}
+                    </h2>
+                    <p>
+                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                      turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+                      sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+                      mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien
+                      ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae,
+                      ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+                      ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis
+                      pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus,
+                      tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat
+                      volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus,
+                      metus
+                    </p>
+                  </section>
+                ))}
+              </div>
+            </div>
+          </article>
+
+          <div className="flex w-full flex-col items-center py-8">
+            <div className="maxw-page w-full text-center md:text-end">
+              [Link to specific article page goes here]
+            </div>
           </div>
-        </div>
-
-        <div className="flex w-full flex-col items-center py-8">
-          <div className="maxw-page w-full text-center md:text-end">
-            [Link to specific article page goes here]
-          </div>
-        </div>
+        </main>
       </div>
 
       <footer className="bg-background-1 flex w-full flex-col items-center">
