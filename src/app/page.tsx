e27e8523641d9f573b9a9327fba7d49a7317bb2f
Khinshan Khan <shan@uptogood.dev>
2025-10-06 20:35:41 -0400

    port over and figure out prose styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,39 +45,44 @@ function ArticleContent() {
         <EdgeFade direction="top" className="z-2 relative h-12" />
 
         <div className="bg-background-2 z-2 relative isolate flex w-full grow flex-col items-center justify-center">
-          <div className="maxw-content relative flex w-full grow flex-col gap-2 py-14">
-            <section aria-labelledby="introduction" className="flex flex-col gap-2">
-              <h2 id="introduction" className="sr-only pt-8 text-2xl font-bold">
+          <div className="maxw-content relative w-full grow py-14">
+            <section aria-labelledby="introduction" className="prose">
+              <H2 id="introduction" className="sr-only">
                 Introduction
-              </h2>
+              </H2>
 
               <p>Hello there. Content goes here [gets sourced from mdx files later].</p>
             </section>
 
             {[...Array(3).keys()].map((num) => (
-              <section key={num} aria-labelledby={`section-${num}`} className="flex flex-col gap-4">
-                <H2 id={`section-${num}`} className="max-w-[75ch] pt-8">
+              <section key={num} aria-labelledby={`section-${num}`} className="prose">
+                <H2 id={`section-${num}`} className="max-w-[75ch]">
                   {`Section ${num}`}
                 </H2>
-                <Paragraph className="max-w-[75ch]">
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
-                  turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit
-                  amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi
-                  vitae est. Mauris placerat eleifend leo.
-                </Paragraph>
 
-                <figure className="flex w-full flex-col items-center justify-center gap-4">
-                  <img
-                    className="size-72"
-                    src="https://1e709a32-0bf5-49ff-b976-d9f66716a6af.mdnplay.dev/shared-assets/images/examples/grapefruit-slice.jpg"
-                    alt="Grapefruit slice atop a pile of other slices"
-                  />
+                {[...Array(3).keys()].map((j) => (
+                  <Paragraph key={j} className="max-w-[75ch]">
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                    turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+                    sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+                    mi vitae est. Mauris placerat eleifend leo.
+                  </Paragraph>
+                ))}
 
-                  <figcaption className="text-foreground-muted flex max-w-[55ch] flex-col items-center gap-2 text-sm">
-                    <Paragraph>Figure of Orange</Paragraph>
-                    <Paragraph>Orange you glad?</Paragraph>
-                  </figcaption>
-                </figure>
+                {num % 2 !== 0 && (
+                  <figure className="flex w-full flex-col items-center justify-center gap-4">
+                    <img
+                      className="size-72"
+                      src="https://1e709a32-0bf5-49ff-b976-d9f66716a6af.mdnplay.dev/shared-assets/images/examples/grapefruit-slice.jpg"
+                      alt="Grapefruit slice atop a pile of other slices"
+                    />
+
+                    <figcaption className="text-foreground-muted flex max-w-[55ch] flex-col items-center gap-2 text-sm">
+                      <Paragraph>Figure of Orange</Paragraph>
+                      <Paragraph>Orange you glad?</Paragraph>
+                    </figcaption>
+                  </figure>
+                )}
               </section>
             ))}
           </div>
