c11570d84c18017f1de256154611b7041a533b14
Khinshan Khan <shan@uptogood.dev>
2025-10-11 22:58:13 -0400

    update next config with sane settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  trailingSlash: true,
+  output: "export",
+  images: { unoptimized: true },
+}
 
-export default nextConfig;
+export default nextConfig
