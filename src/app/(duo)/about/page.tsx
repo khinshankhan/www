b5af867aa0b42e56e634ec0f3da4b76423cbc2bf
Khinshan Khan <shan@uptogood.dev>
2025-10-17 02:20:01 -0400

    poc smart toc adaption

--- src/app/(duo)/about/page.tsx
+++ src/app/(duo)/about/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import React from "react"
+import { H2 } from "@/components/design-system/primitives/text"
 import { AppShell } from "@/components/layout/app-shell"
 import { DuoLayout } from "@/components/layout/duo-layout"
+import { Heading, TOC } from "@/components/layout/toc"
 import { WithSidebar } from "@/components/layout/with-sidebar"
 
 const title = "About"
@@ -10,14 +12,67 @@ const description =
   "Exploring the intersections of the online and offline worlds with curiosity, creativity, and a dash of adventure."
 
 export default function Page() {
+  const sectionsCount = 5
+
+  const headings: Heading[] = [
+    { id: "introduction", title: "Introduction", depth: 2 },
+    ...[...Array(sectionsCount).keys()].map((num) => ({
+      id: `section-${num}`,
+      title: `Section ${num}`,
+      depth: 2,
+    })),
+  ]
+
   return (
-    <AppShell>
+    <AppShell
+      siteHeaderProps={{
+        edgeFadeProps: {
+          className: "hidden xl:block",
+        },
+      }}
+    >
       <DuoLayout title={title} description={description} ghPath={"/src/app/(duo)/about/page.tsx"}>
-        <WithSidebar sidebar={null} direction="right">
+        <WithSidebar sidebar={<TOC headings={headings} />} direction="right">
           <div className="mx-auto min-w-full">
-            <div className="prose text-foreground-muted max-w-none">
-              <p>Coming soon...</p>
-            </div>
+            <section aria-labelledby="introduction" className="prose">
+              <H2 id="introduction" className="sr-only scroll-mt-5">
+                Introduction
+              </H2>
+              <p>
+                Introductory paragraph goes here. No sweat, should be short and sweet, super simple
+                (famous last words). Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies
+                eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
+                ultricies mi vitae est. Mauris placerat eleifend leo.
+              </p>
+            </section>
+
+            {[...Array(sectionsCount).keys()].map((num) => {
+              return (
+                <section key={num} aria-labelledby={`section-${num}`} className="prose">
+                  <H2 id={`section-${num}`} className="scroll-mt-5">{`Section ${num}`}</H2>
+
+                  <p className="">
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                    turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+                    sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+                    mi vitae est. Mauris placerat eleifend leo.
+                  </p>
+                  <p className="">
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                    turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+                    sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+                    mi vitae est. Mauris placerat eleifend leo.
+                  </p>
+                  <p className="">
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                    turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+                    sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+                    mi vitae est. Mauris placerat eleifend leo.
+                  </p>
+                </section>
+              )
+            })}
           </div>
         </WithSidebar>
       </DuoLayout>
