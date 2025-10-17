b3597be4b95e8f20c8383f70fce5e81442f11922
Khinshan Khan <shan@uptogood.dev>
2025-10-17 00:06:59 -0400

    track active anchors

--- src/components/layout/toc.tsx
+++ src/components/layout/toc.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 import React from "react"
+import {
+  ActiveAnchorsProvider,
+  useActiveAnchors,
+} from "@/components/design-system/patterns/view-observers/active-anchors"
 import { ListTree } from "@/components/design-system/primitives/icon"
 import { H2 } from "@/components/design-system/primitives/text"
 import { cn } from "@/lib/utils"
@@ -11,45 +15,62 @@ export interface Heading {
   depth: number
 }
 
+interface TocItemProps {
+  heading: Heading
+}
+function TocItem({ heading }: TocItemProps) {
+  const { activeId } = useActiveAnchors()
+  const isActive = activeId === heading.id
+
+  // TODO: implement this
+  return (
+    <li>
+      <a
+        className={cn("pl-4 hover:underline", isActive && "text-accent-11")}
+        href={`#${heading.id}`}
+      >
+        {heading.title}
+      </a>
+    </li>
+  )
+}
+
 interface TableOfContentsProps {
   headings?: Heading[]
   className?: string
 }
 
-// TODO: implement this
 export function TOC({ headings = [], className = "" }: TableOfContentsProps) {
   return (
-    <nav
-      aria-label="Table of contents"
-      className={cn(
-        "bg-background-2 xl:bg-background-2/70 sticky top-0 w-full rounded-lg rounded-t-none px-2 py-3 backdrop-blur xl:-mt-3",
-        className
-      )}
-    >
-      <H2
-        variant="h5"
-        className="text-foreground mb-4 mt-0 flex flex-row items-center justify-start gap-2"
+    <ActiveAnchorsProvider ids={headings.map((h) => h.id)}>
+      <nav
+        aria-label="Table of contents"
+        className={cn(
+          "bg-background-2 xl:bg-background-2/70 sticky top-0 w-full rounded-lg rounded-t-none px-2 py-3 backdrop-blur xl:-mt-3",
+          className
+        )}
       >
-        <span>
-          <ListTree />
-        </span>
-        On this page
-      </H2>
+        <H2
+          variant="h5"
+          className="text-foreground mb-4 mt-0 flex flex-row items-center justify-start gap-2"
+        >
+          <span>
+            <ListTree />
+          </span>
+          On this page
+        </H2>
 
-      <ul
-        className="border-background-1 flex flex-col gap-2"
-        style={{
-          borderLeftWidth: "1.5px",
-        }}
-      >
-        {headings.map((heading) => (
-          <li key={heading.id}>
-            <a className="pl-4 hover:underline" href={`#${heading.id}`}>
-              {heading.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+        <ul
+          className="border-background-1 flex flex-col gap-2"
+          style={{
+            borderLeftWidth: "1.5px",
+          }}
+        >
+          {headings.map((heading) => (
+            <TocItem key={heading.id} heading={heading} />
+          ))}
+        </ul>
+      </nav>
+    </ActiveAnchorsProvider>
   )
 }
