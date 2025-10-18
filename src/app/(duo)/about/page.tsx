67b67bda88d1cf8a8c31d5c37b3425be3048d597
Khinshan Khan <shan@uptogood.dev>
2025-10-17 23:20:08 -0400

    apply anchor links to headings

--- src/app/(duo)/about/page.tsx
+++ src/app/(duo)/about/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import { Link } from "@/components/design-system/primitives/link"
 import { H2 } from "@/components/design-system/primitives/text"
 import { AppShell } from "@/components/layout/app-shell"
 import { DuoLayout } from "@/components/layout/duo-layout"
@@ -35,7 +36,7 @@ export default function Page() {
         <WithSidebar sidebar={<TOC headings={headings} />} direction="right">
           <div className="mx-auto min-w-full">
             <section aria-labelledby="introduction" className="prose">
-              <H2 id="introduction" className="sr-only scroll-mt-5">
+              <H2 id="introduction" className="sr-only scroll-mt-72">
                 Introduction
               </H2>
               <p>
@@ -45,12 +46,26 @@ export default function Page() {
                 eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
                 ultricies mi vitae est. Mauris placerat eleifend leo.
               </p>
+              <p className="">
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
+                ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+                Mauris placerat eleifend leo.
+              </p>
+              <p className="">
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
+                ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+                Mauris placerat eleifend leo.
+              </p>
             </section>
 
             {[...Array(sectionsCount).keys()].map((num) => {
               return (
                 <section key={num} aria-labelledby={`section-${num}`} className="prose">
-                  <H2 id={`section-${num}`} className="scroll-mt-5">{`Section ${num}`}</H2>
+                  <H2 id={`section-${num}`} className="scroll-mt-28">
+                    <Link className="anchor-link">{`Section ${num}`}</Link>
+                  </H2>
 
                   <p className="">
                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
