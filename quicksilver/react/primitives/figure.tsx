6851f3b4219808a2b394a9f5de5fec3c352840f8
Khinshan Khan <shan@uptogood.dev>
2025-10-22 17:59:00 -0400

    split out figcaption

--- quicksilver/react/primitives/figure.tsx
+++ quicksilver/react/primitives/figure.tsx
@@ -1,6 +1,4 @@
 import React, { type ComponentPropsWithRef, type ReactNode } from "react"
-import { cn } from "@/quicksilver/lib/classname"
-import { textVariants } from "./text.variants"
 
 export interface FigureProps extends ComponentPropsWithRef<"figure"> {
   children: ReactNode
@@ -8,23 +6,4 @@ export interface FigureProps extends ComponentPropsWithRef<"figure"> {
 
 export function Figure({ children, ...props }: FigureProps) {
   return <figure {...props}>{children}</figure>
-}
-
-export interface FigcaptionProps extends ComponentPropsWithRef<"figcaption"> {
-  children: ReactNode
-}
-
-export function Figcaption({ children, className = "", ...props }: FigcaptionProps) {
-  return (
-    <figcaption
-      className={cn(
-        textVariants({ variant: "small" }),
-        "mt-4 text-center text-balance text-foreground-muted",
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </figcaption>
-  )
 }
