185b0ab68c14aee29bb32f682bf7a1732d24e33d
Khinshan Khan <shan@uptogood.dev>
2025-10-24 17:58:43 -0400

    migrate about page to markdown

---
+++ app/(duotone)/[...slug]/page.tsx
@@ -0,0 +1,77 @@
+import React from "react"
+import type { Metadata } from "next"
+import { DuotoneLayout } from "@/components/layouts/duotone"
+import { Header } from "@/components/layouts/elements/header"
+import { Shell } from "@/components/layouts/elements/shell"
+import { TOC } from "@/components/layouts/sidebars/toc"
+import { WithSidebar } from "@/components/layouts/with-sidebar"
+import { MDXRenderer } from "@/components/mdx-renderer"
+import { getCachedAllContentData, getCachedContentDataBySlug } from "@/lib/content/source"
+import { createMetadata, processMarkdownAttribute } from "@/lib/seo/open-graph"
+
+export function generateStaticParams() {
+  const slugsParts = getCachedAllContentData().map((contentData) => {
+    return {
+      slug: contentData.slug.split("/").filter((part) => part.trim() !== ""),
+    }
+  })
+
+  return slugsParts
+}
+
+export default async function Page({ params }: { params: Promise<{ slug: string[] }> }) {
+  const fileSlug = (await params).slug
+  const realFileSlug = `/${fileSlug.join("/")}/`
+  const contentData = getCachedContentDataBySlug(realFileSlug)
+  if (!contentData) {
+    throw new Error(`Could not find metadata for ${realFileSlug}`)
+  }
+
+  return (
+    <Shell
+      header={
+        <Header
+          edgeFadeProps={{
+            className: contentData.frontmatter.showToc ? "hidden xl:block" : "",
+          }}
+        />
+      }
+    >
+      <DuotoneLayout
+        title={contentData.frontmatter.title}
+        description={contentData.frontmatter.description}
+        ghPath={contentData.computed.ghSlug}
+      >
+        <WithSidebar
+          sidebar={
+            contentData.frontmatter.showToc ? <TOC headings={contentData.computed.toc} /> : null
+          }
+          direction="right"
+        >
+          <div className="mx-auto min-w-full">
+            <MDXRenderer source={contentData.content} />
+          </div>
+        </WithSidebar>
+      </DuotoneLayout>
+    </Shell>
+  )
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>
+}): Promise<Metadata | undefined> {
+  const fileSlug = (await params).slug
+  const realFileSlug = `/${fileSlug.join("/")}/`
+  const contentData = getCachedContentDataBySlug(realFileSlug)
+  if (!contentData) {
+    throw new Error(`Could not find metadata for ${realFileSlug}`)
+  }
+
+  return createMetadata({
+    title: contentData.frontmatter.title,
+    description: processMarkdownAttribute(contentData.frontmatter.description),
+    slug: `${fileSlug.join("/")}/`,
+  })
+}
