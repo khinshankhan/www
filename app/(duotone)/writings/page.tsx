2e4bd7f0422c7ab6bcf950ba2d43203fd891ae68
Khinshan Khan <shan@uptogood.dev>
2025-10-24 20:39:04 -0400

    simple writings list

--- app/(duotone)/writings/page.tsx
+++ app/(duotone)/writings/page.tsx
@@ -3,8 +3,10 @@ import type { Metadata } from "next"
 import { DuotoneLayout } from "@/components/layouts/duotone"
 import { Shell } from "@/components/layouts/elements/shell"
 import { WithSidebar } from "@/components/layouts/with-sidebar"
+import { getAllContentData } from "@/lib/content/source"
 import { createMetadata, processMarkdownAttribute } from "@/lib/seo/open-graph"
 import { Callout } from "@/quicksilver/react/primitives/callout"
+import { Link } from "@/quicksilver/react/primitives/link"
 
 const title = "Writings"
 const description =
@@ -12,16 +14,35 @@ const description =
 const ghPath = "/app/(duotone)/writings/page.tsx"
 const slug = "/writings"
 
-export default function Page() {
+async function getPostsList() {
+  const list = await getAllContentData()
+  return list.filter((data) => data.source === "posts")
+}
+
+export default async function Page() {
+  const posts = await getPostsList()
+
   return (
     <Shell>
       <DuotoneLayout title={title} description={description} ghPath={ghPath}>
         <WithSidebar sidebar={null} direction="right">
           <div className="mx-auto min-w-full">
-            <Callout variant="note" title="Work in Progress" icon={null}>
-              I am currently working on converting my previous articles from different formats to
-              markdown. This process will take some time. Stay tuned... coming soon!
-            </Callout>
+            <div className="flex grow flex-col gap-4">
+              <Callout variant="note" title="Work in Progress" icon={null}>
+                I am currently working on converting my previous articles from different formats to
+                markdown. This process will take some time. Stay tuned... coming soon!
+              </Callout>
+
+              <div className="prose">
+                <ul>
+                  {posts.map((page) => (
+                    <li key={page.computed.ghSlug}>
+                      <Link href={page.slug}>{page.frontmatter.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </WithSidebar>
       </DuotoneLayout>
