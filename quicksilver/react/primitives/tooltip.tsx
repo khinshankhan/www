3b9690b7fa86c82a6acdf1cc4b5c930697f4a256
Khinshan Khan <shan@uptogood.dev>
2025-10-29 23:40:41 -0400

    tidy up tooltip arrow and svg classes

--- quicksilver/react/primitives/tooltip.tsx
+++ quicksilver/react/primitives/tooltip.tsx
@@ -42,11 +42,11 @@ export function TooltipContent({
           )}
         >
           {children}
-          <TooltipArrow className="fill-[canvas] data-[side=bottom]:-top-2 data-[side=left]:-right-2 data-[side=left]:rotate-90 data-[side=right]:-left-2 data-[side=right]:-rotate-90 data-[side=top]:-bottom-2">
+          <TooltipArrow className="data-[side=bottom]:-top-2 data-[side=left]:-right-[14px] data-[side=left]:rotate-90 data-[side=right]:-left-[14px] data-[side=right]:-rotate-90 data-[side=top]:-bottom-2 data-[side=top]:rotate-180">
             <svg width="20" height="10" viewBox="0 0 20 10" fill="none">
               <path
                 d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
-                className="fill-[canvas]"
+                className="fill-stark-contrast"
               />
             </svg>
           </TooltipArrow>
